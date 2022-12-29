19be7ab3076e2ed70f78bd528ec919b102605f06
tabalong <ikhwchemist@gmail.com>
2022-12-29 10:00:12 +0800

    menambah meta

---
+++ components/Meta.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta property="og:image" content="/image/logolb3r.png" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="100" />
+    <meta
+      property="og:description"
+      content="Web khusus siswa LB3R berisi ketentuan-ketentuan (aturan) selama bergabung dengan LB3R"></meta>
+    <title>Peraturan LB3R</title>
+  </Head>
+);
+
+export default Meta;
