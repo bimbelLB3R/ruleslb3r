20ce6f11f9847ff4ab81a6eb2d77bf4085c7a9ca
tabalong <ikhwchemist@gmail.com>
2023-01-01 18:12:44 +0800

    remove env github

--- components/Wa.js
+++ components/Wa.js
@@ -1,3 +1,5 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 const Wa = () => {
   return (
     <div>
@@ -14,6 +16,19 @@ const Wa = () => {
           </svg> */}
           <img src="image/iconkritiksaran.png" className="w-12 z-50" />
         </a>
+        {/* <button onClick={signIn}>
+          <div className="flex  items-center text-gray-400 hover:cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="#EA580C"
+              className="bi bi-chat-right-dots-fill"
+              viewBox="0 0 16 16">
+              <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+            </svg>
+          </div>
+        </button> */}
       </div>
     </div>
   );
