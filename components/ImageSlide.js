25d0987ef8f098191e6acddf5cd90b5216141cd6
tabalong <ikhwchemist@gmail.com>
2023-01-08 12:24:40 +0800

    ubah format gambar

--- components/ImageSlide.js
+++ components/ImageSlide.js
@@ -11,31 +11,31 @@ export default class ImageSlide extends Component {
           {/* <h2>NextJs Carousel - GeeksforGeeks</h2> */}
           <Carousel>
             <div>
-              <img src="image/image1.svg" alt="image1" />
+              <img src="image/image1.png" alt="image1" />
               <p className="legend">SEMINAR BAKAT SMA N 1 TANJUNG</p>
             </div>
             <div>
-              <img src="image/image2.svg" alt="image2" />
+              <img src="image/image2.png" alt="image2" />
               <p className="legend">KELAS PEMROGRAMAN WEBSITE</p>
             </div>
             <div>
-              <img src="image/image3.svg" alt="image3" />
+              <img src="image/image3.png" alt="image3" />
               <p className="legend">TRY OUT UJIAN MASUK KAMPUS</p>
             </div>
             <div>
-              <img src="image/image4.svg" alt="image4" />
+              <img src="image/image4.png" alt="image4" />
               <p className="legend">KANTOR UTAMA BIMBEL LB3R</p>
             </div>
             <div>
-              <img src="image/image5.svg" alt="image4" />
+              <img src="image/image5.png" alt="image4" />
               <p className="legend">PENERAPAN TEKNOLOGI TERKINI </p>
             </div>
             <div>
-              <img src="image/image6.svg" alt="image4" />
+              <img src="image/image6.png" alt="image5" />
               <p className="legend">BEBERAPA PENGAJAR PEREMPUAN DI LB3R</p>
             </div>
             <div>
-              <img src="image/image7.svg" alt="image4" />
+              <img src="image/image7.png" alt="image7" />
               <p className="legend">SALAH SATU SISWA MERAYAKAN ULANG TAHUN</p>
             </div>
           </Carousel>
