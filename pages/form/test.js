5ce32cad785dee60a2af81d3565ea8cbc0d280ea
tabalong <ikhwchemist@gmail.com>
2023-01-23 22:13:06 +0800

    succes add latex

--- pages/form/test.js
+++ pages/form/test.js
@@ -1,10 +1,23 @@
-import Timer from '../../components/Timer';
+import Head from 'next/head';
+import React from 'react';
+import Latex from 'react-latex';
 
 const test = () => {
+  const fraction = `$$\\left (\\frac {x}{y} \\right )^2$$`;
   return (
-    <div>
-      <Timer />
-    </div>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+          integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <div className="text-xl text-red-900">
+        <Latex>{fraction}</Latex>
+      </div>
+    </>
   );
 };
 
