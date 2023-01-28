5e148b58cb2f03b4371893673aebb6630c513620
tabalong <ikhwchemist@gmail.com>
2023-01-29 02:43:46 +0800

    hapus icon favicon

--- components/Meta.js
+++ components/Meta.js
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <meta property="og:image" content="/image/logolb3r.png" />
+    <meta property="og:image" content="/image/image4.png" />
     <meta property="og:image:width" content="400" />
-    <meta property="og:image:height" content="100" />
+    <meta property="og:image:height" content="400" />
     <meta
       property="og:description"
-      content="Web khusus siswa LB3R berisi ketentuan-ketentuan (aturan) selama bergabung dengan LB3R"></meta>
-    <title>Peraturan LB3R</title>
+      content="Yuk..gabung LB3R, bimbingan belajar mulai SD,SMP,SMA, dan Alumni."></meta>
+    <title>LB3R |OFFICIAL</title>
   </Head>
 );
 
