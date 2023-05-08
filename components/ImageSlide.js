e4750362c36f97a8737efaa22851829592cb5447
tabalong <ikhwchemist@gmail.com>
2023-05-09 00:20:40 +0800

    mengubah Image pada imageslide dan false thumbnail

--- components/ImageSlide.js
+++ components/ImageSlide.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import Welcome from './Welcome';
-// import Image from 'next/image';
+import Image from 'next/image';
 
 export default class ImageSlide extends Component {
   render() {
@@ -10,68 +10,75 @@ export default class ImageSlide extends Component {
       <div className="flex items-center justify-center">
         <div className="md:max-w-2xl  overflow-hidden">
           {/* <h2>NextJs Carousel - GeeksforGeeks</h2> */}
-          <Carousel>
+          <Carousel showArrows={true} showThumbs={false}>
             <div>
-              {/* <Image
-                src="/image/image1.png"
+              <Image
+                src="/image/image1.webp"
                 alt="Foto Seminar Bakat"
-                layout="fill"
-              /> */}
-              <img src="image/image1.webp" alt="Foto Seminar Bakat" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image1.webp" alt="Foto Seminar Bakat" /> */}
               <p className="legend">SEMINAR BAKAT DAN PEMILIHAN JURUSAN</p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image2.webp"
                 alt="Foto Kelas Programming"
-                layout="fill"
-              /> */}
-              <img src="image/image2.webp" alt="Foto Kelas Programming" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image2.webp" alt="Foto Kelas Programming" /> */}
               <p className="legend">KELAS PEMROGRAMAN WEB</p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image3.webp"
                 alt="Foto Kelas UTBK"
-                layout="fill"
-              /> */}
-              <img src="image/image3.webp" alt="Foto Kelas UTBK" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image3.webp" alt="Foto Kelas UTBK" /> */}
               <p className="legend">KELAS INTENSIF UTBK SNBT</p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image4.webp"
                 alt="Foto Kantor Bimbel"
-                layout="fill"
-              /> */}
-              <img src="image/image4.webp" alt="Foto Kantor Bimbel" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image4.webp" alt="Foto Kantor Bimbel" /> */}
               <p className="legend">KANTOR UTAMA BIMBEL LB3R</p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image5.webp"
                 alt="Foto presensi QRcode"
-                layout="fill"
-              /> */}
-              <img src="image/image5.webp" alt="Foto presensi QRcode" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image5.webp" alt="Foto presensi QRcode" /> */}
               <p className="legend">PRESENSI DENGAN QRQODE </p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image6.webp"
                 alt="Foto Pengajar"
-                layout="fill"
-              /> */}
-              <img src="image/image6.webp" alt="Foto Pengajar LB3R" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image6.webp" alt="Foto Pengajar LB3R" /> */}
               <p className="legend">BEBERAPA PENGAJAR PEREMPUAN DI LB3R</p>
             </div>
             <div>
-              {/* <Image
+              <Image
                 src="/image/image7.webp"
                 alt="Foto Siswa Ultah"
-                layout="fill"
-              /> */}
-              <img src="image/image7.webp" alt="Foto Siswa LB3R Ultah" />
+                width="500px"
+                height="500px"
+              />
+              {/* <img src="image/image7.webp" alt="Foto Siswa LB3R Ultah" /> */}
               <p className="legend">SALAH SATU SISWA MERAYAKAN ULANG TAHUN</p>
             </div>
           </Carousel>
