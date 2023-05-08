64d11b18e7d2463306fddd5de685462145d7bd35
tabalong <ikhwchemist@gmail.com>
2023-05-08 23:18:36 +0800

    dynamic layout di indexJs

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <footer className="bg-gray-900 ">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div className="md:flex md:justify-between">
+          <div className="md:flex md:justify-evenly">
             <div className="mb-6 md:mb-0">
               <a href="/" className="flex items-center">
                 <Image
--- components/Meta.js
+++ components/Meta.js
@@ -15,6 +15,11 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>LB3R |OFFICIAL</title>
     <link rel="icon" type="image/png" sizes="4x16" href="image/logolb3r.png" />
+    <meta
+      name="description"
+      content="Pembelajaran di Bimbel LB3R sangat fleksibel sesuai kebutuhan siswa. Siswa dapat membuat kelas sendiri dan mengatur jadwalnya. Pembayaran les di bimbel LB3R juga sangat fleksibel, yaitu sistem bulanan. Siswa dapat berhenti sementara dan kemudian lanjut bulan berikutnya. Dari segi pelayanan dan tempat belajar, kami mengutamakan kenyamanan."
+      key="desc"
+    />
   </Head>
 );
 
