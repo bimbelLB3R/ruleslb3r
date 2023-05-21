67bef0daf5cff1a3da8cbc85675c4ca84d9b5886
bimbellb3r <bimbellb3r@gmail.com>
2023-05-21 14:38:20 +0800

    error next auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from 'next-auth/providers/google';
 // import { Providers } from 'next-auth/providers';
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
