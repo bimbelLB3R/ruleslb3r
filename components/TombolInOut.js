1e559e479663778a9e1673b9d7e67df3760bd1ac
bimbellb3r <bimbellb3r@gmail.com>
2023-05-21 23:44:17 +0800

    styling tombol

--- components/TombolInOut.js
+++ components/TombolInOut.js
@@ -8,7 +8,7 @@ export default function TombolInOut() {
     return (
       <>
         <div className="flex space-x-2 items-center">
-          <p>Signed in as {session.user.name}</p>
+          <p className="text-xs">Hai,{session.user.name}</p>
           <Image
             src={session.user.image}
             width={36}
@@ -16,15 +16,17 @@ export default function TombolInOut() {
             alt="userImage"
             className="rounded-full"
           />
-          <div className="border-1 p-1 border-orange-900">
-            <button onClick={() => signOut()}>Sign out</button>
+          <div className="underline">
+            <button onClick={() => signOut()}>
+              <p className="text-xs">Sign out</p>
+            </button>
           </div>
         </div>
       </>
     );
   }
   return (
-    <div className="flex bg-orange-900 w-full space-x-2 items-center text-slate-100 p-2 ">
+    <div className="flex bg-orange-900 w-full space-x-2 items-center text-slate-100 p-2 text-xs">
       <p>Not signed in</p>
       <button onClick={() => signIn()}>Sign in</button>
     </div>
