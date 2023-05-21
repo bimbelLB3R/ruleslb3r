2862ab9200ea982ebe4fc0eac8c7da282bfd773b
bimbellb3r <bimbellb3r@gmail.com>
2023-05-21 22:56:16 +0800

    menambah term ,privacy

---
+++ pages/user/privacy.js
@@ -0,0 +1,73 @@
+export default function Privacy() {
+  return (
+    <>
+      <p>Privacy Policy Terakhir diperbarui: 2023-05-21</p>
+      <p>
+        Kami menghargai privasi Anda dan berkomitmen untuk melindungi informasi
+        pribadi Anda. Kebijakan privasi ini menjelaskan bagaimana kami
+        mengumpulkan, menggunakan, dan melindungi informasi yang Anda berikan
+        saat mengunjungi situs web kami.
+      </p>
+      <p>1. Informasi yang Kami Kumpulkan </p>
+      <p>
+        Kami dapat mengumpulkan informasi pribadi seperti nama, alamat email,
+        dan informasi kontak lainnya yang Anda berikan secara sukarela saat
+        mengisi formulir pendaftaran, mengirim komentar, atau berinteraksi
+        dengan situs web kami. Selain itu, kami juga dapat mengumpulkan
+        informasi non-pribadi seperti data demografis, preferensi pengguna, dan
+        informasi teknis tentang perangkat dan koneksi internet yang Anda
+        gunakan untuk mengakses situs web kami.{' '}
+      </p>
+      <p>2. Penggunaan Informasi</p>
+      <p>
+        nformasi pribadi yang Anda berikan akan digunakan untuk tujuan yang
+        telah dijelaskan pada saat pengumpulan. Kami dapat menggunakan informasi
+        tersebut untuk mengirimkan konten, pembaruan, atau informasi terkait
+        lainnya yang mungkin menarik bagi Anda. Informasi non-pribadi yang kami
+        kumpulkan dapat digunakan untuk menganalisis penggunaan situs web kami,
+        meningkatkan pengalaman pengguna, dan memberikan konten yang lebih
+        relevan.{' '}
+      </p>
+      I<p>3. Pengungkapan Informasi kepada Pihak Ketiga </p>I
+      <p>
+        Kami tidak akan mengungkapkan informasi pribadi Anda kepada pihak ketiga
+        tanpa persetujuan Anda, kecuali jika diwajibkan oleh hukum atau jika
+        diperlukan untuk menjaga keamanan dan integritas situs web kami. Kami
+        dapat menggunakan penyedia layanan pihak ketiga untuk membantu
+        operasional situs web kami atau memberikan layanan kepada Anda. Namun,
+        penyedia layanan ini dilarang menggunakan informasi pribadi Anda untuk
+        tujuan mereka sendiri dan diharuskan menjaga kerahasiaan informasi
+        tersebut.{' '}
+      </p>
+      I<p>4. Keamanan Informasi </p>I
+      <p>
+        Kami menjaga keamanan informasi Anda dengan menerapkan tindakan
+        pencegahan teknis dan kebijakan yang sesuai. Namun, tidak ada metode
+        transmisi atau penyimpanan data yang sepenuhnya aman. Oleh karena itu,
+        kami tidak dapat menjamin keamanan mutlak dari informasi yang Anda
+        berikan kepada kami.{' '}
+      </p>
+      I<p>5. Tautan ke Situs Web Eksternal</p>I
+      <p>
+        Situs web kami mungkin berisi tautan ke situs web eksternal yang tidak
+        dioperasikan oleh kami. Kami tidak bertanggung jawab atas kebijakan
+        privasi atau konten dari situs web eksternal tersebut. Kami mendorong
+        Anda untuk membaca kebijakan privasi setiap situs web yang Anda
+        kunjungi.{' '}
+      </p>
+      <p>6.Perubahan pada Kebijakan Privasi </p>I
+      <p>
+        Kami dapat memperbarui kebijakan privasi ini dari waktu ke waktu. Setiap
+        perubahan akan diposting di situs web ini dengan tanggal perubahan yang
+        diperbarui. Anda disarankan untuk secara teratur memeriksa kebijakan
+        privasi ini.{' '}
+      </p>
+      <p>7. Hubungi Kami </p>I
+      <p>
+        Jika Anda memiliki pertanyaan atau komentar tentang kebijakan privasi
+        ini, silakan hubungi
+      </p>
+      I
+    </>
+  );
+}
---
+++ pages/user/term.js
@@ -0,0 +1,38 @@
+export default function Term() {
+  return (
+    <p>
+      Terms of Service Terakhir diperbarui: [Tanggal Terakhir Diperbarui]
+      Selamat datang di situs web [Nama Situs]. Mohon baca Syarat dan Ketentuan
+      Layanan ini dengan cermat sebelum menggunakan situs web kami. Penerimaan
+      Syarat dan Ketentuan Dengan mengakses dan menggunakan situs web ini, Anda
+      dianggap telah membaca, memahami, dan menerima semua syarat dan ketentuan
+      yang tercantum dalam dokumen ini. Jika Anda tidak setuju dengan syarat dan
+      ketentuan ini, harap jangan menggunakan situs web kami. Penggunaan Situs
+      a. Anda diperbolehkan menggunakan situs web ini untuk tujuan pribadi,
+      non-komersial, dan sesuai dengan hukum yang berlaku. b. Anda setuju untuk
+      tidak melakukan tindakan berikut: i. Melanggar hak kekayaan intelektual
+      kami atau pihak ketiga. ii. Mengganggu, merusak, atau memanipulasi situs
+      web atau infrastruktur yang terkait. iii. Mengirimkan konten yang
+      melanggar hukum, mengandung materi yang tidak pantas, atau dapat merugikan
+      orang lain. iv. Mengakses data pribadi pengguna lain tanpa izin. Kekayaan
+      Intelektual a. Seluruh konten yang terdapat di situs web ini, termasuk
+      tetapi tidak terbatas pada teks, gambar, grafik, logo, dan merek dagang,
+      dilindungi oleh hukum kekayaan intelektual yang berlaku. b. Anda tidak
+      diizinkan untuk menggunakan, menggandakan, mendistribusikan, atau
+      memanipulasi konten situs web tanpa izin tertulis dari pemiliknya.
+      Penafian Tanggung Jawab a. Situs web ini disediakan "apa adanya" dan kami
+      tidak memberikan jaminan eksplisit atau implisit mengenai keakuratan,
+      keandalan, atau ketersediaan situs web ini. b. Kami tidak bertanggung
+      jawab atas kerugian atau kerusakan yang timbul akibat penggunaan situs web
+      ini atau ketergantungan pada konten yang terdapat di situs web ini.
+      Perubahan Syarat dan Ketentuan Kami berhak untuk mengubah atau memperbarui
+      Syarat dan Ketentuan Layanan ini sesuai kebijakan kami. Perubahan tersebut
+      akan efektif setelah diperbarui di situs web ini. Anda bertanggung jawab
+      untuk secara teratur memeriksa perubahan pada Syarat dan Ketentuan ini.
+      Hukum yang Berlaku Syarat dan Ketentuan ini tunduk pada hukum yang berlaku
+      di [Negara/Provinsi]. Setiap perselisihan yang timbul terkait dengan
+      penggunaan situs web ini akan diselesaikan di pengadilan yang berwenang di
+      [Kota].
+    </p>
+  );
+}
