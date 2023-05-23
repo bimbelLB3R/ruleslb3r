54b2795e0906d4d75ed6a120a81170e5b999bcf3
bimbellb3r <bimbellb3r@gmail.com>
2023-05-24 06:44:38 +0800

    styling blogs again

--- components/ArtikelTutorial.js
+++ components/ArtikelTutorial.js
@@ -2,16 +2,16 @@ import Link from 'next/link';
 export default function ArtikelTutorial({ allTutorial }) {
   return (
     <>
-      <div className="mt-10 flex  border border-l-8 p-1 m-2 border-slate-300 md:max-w-xl md:m-auto">
+      <div className="mt-10 flex border-l-8 p-1 m-2 border-orange-900 md:max-w-xl md:m-auto">
         <div className="">
-          <p className="font-bold">ARTIKEL TUTORIAL</p>
+          <p className="font-bold mb-2">ARTIKEL TUTORIAL</p>
           <div>
             {allTutorial.slice(0, 2).map((tutorial) => (
               <Link
                 key={tutorial.id}
                 href={`/blogs/${tutorial.slug}`}
-                className="hover:underline">
-                <h1>{tutorial.title}</h1>
+                className="hover:underline leading-none">
+                <h1 className="mb-[6px] text-blue-900">{tutorial.title}</h1>
               </Link>
             ))}
           </div>
--- components/BeritaTerbaru.js
+++ components/BeritaTerbaru.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function BeritaTerbaru({ allPost }) {
   return (
     <>
-      <div className=" flex justify-center items-center mt-20 md:mt-10">
+      <div className=" flex justify-center items-center mt-[50px] md:mt-10">
         <div>
           {allPost.map((post, index) => (
             <div key={post.id} className="w-full flex justify-center m-auto">
@@ -12,8 +12,8 @@ export default function BeritaTerbaru({ allPost }) {
                 href={`/blogs/${post.slug}`}
                 className={
                   index !== 0
-                    ? 'flex p-2 mt-1 mb-2 mr-5 ml-5 border border-b-slate-300 rounded w-full space-x-1'
-                    : 'p-2 mt-5 mb-5 mr-5 ml-5 border border-b-slate-300 rounded'
+                    ? 'flex items-center p-1 md:p-2 mt-1 mb-2 md:mr-5 md:ml-5 border-b-1 hover:shadow bg-slate-50 border-b-slate-300  w-full space-x-1'
+                    : 'p-1 md:p-2 mt-5 mb-5 md:mr-5 md:ml-5 border-b-1 bg-slate-50 text-center leading-none border-b-slate-300 '
                 }>
                 <div>
                   <Image
@@ -22,16 +22,12 @@ export default function BeritaTerbaru({ allPost }) {
                     height={index === 0 ? 300 : 150}
                     alt={post.id}
                     priority={true}
-                    className={
-                      index === 0 ? 'rounded' : 'w-[100px] h-[100px] rounded'
-                    }
+                    className={index === 0 ? '' : 'w-[100px] h-[100px] '}
                   />
                 </div>
-                <div>
+                <div className="leading-none">
                   <div>
-                    <h1 className="font-semibold hover:bg-gray-300 p-1">
-                      {post.title}
-                    </h1>
+                    <h1 className="font-semibold  p-1">{post.title}</h1>
                   </div>
                   <div>Penulis : {post.writer}</div>
                   <p>{post.createdAt}</p>
