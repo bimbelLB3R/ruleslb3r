3f59b7e8d607d4526105db089ab15884ec42f234
bimbellb3r <bimbellb3r@gmail.com>
2023-05-24 21:10:51 +0800

    edit meta

--- components/MetaForBlog.js
+++ components/MetaForBlog.js
@@ -5,14 +5,12 @@ export default function MetaForBlog({ detailPost }) {
   //   console.log(ogTitle);
   const ogDesc = detailPost.description;
   const ogImage = `${process.env.PUBLIC_URL}/og-images/${detailPost.ogImageName}`;
-  const ogUrl = detailPost.slug;
   return (
     <Head>
+      <meta property="og:image" itemProp="image" content={ogImage} />
       <meta property="og:title" content={ogTitle} />
       <meta property="og:description" content={ogDesc} />
-      <meta property="og:image" content={ogImage} />
-      <meta property="og:url" content={`/blogs/${ogUrl}`} />
-      <meta property="og:type" content="article" />
+      <meta property="og:url" content={process.env.PUBLIC_URL} />
     </Head>
   );
 }
