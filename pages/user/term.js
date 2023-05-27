e6681bb5f1a3231fd65a38ee283d9f66bec92361
bimbellb3r <bimbellb3r@gmail.com>
2023-05-27 11:58:43 +0800

    edit privacy n term

--- pages/user/term.js
+++ pages/user/term.js
@@ -1,38 +1,44 @@
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+import Navbar from '../../components/Navbar';
 export default function Term() {
   return (
-    <p>
-      Terms of Service Terakhir diperbarui: [Tanggal Terakhir Diperbarui]
-      Selamat datang di situs web [Nama Situs]. Mohon baca Syarat dan Ketentuan
-      Layanan ini dengan cermat sebelum menggunakan situs web kami. Penerimaan
-      Syarat dan Ketentuan Dengan mengakses dan menggunakan situs web ini, Anda
-      dianggap telah membaca, memahami, dan menerima semua syarat dan ketentuan
-      yang tercantum dalam dokumen ini. Jika Anda tidak setuju dengan syarat dan
-      ketentuan ini, harap jangan menggunakan situs web kami. Penggunaan Situs
-      a. Anda diperbolehkan menggunakan situs web ini untuk tujuan pribadi,
-      non-komersial, dan sesuai dengan hukum yang berlaku. b. Anda setuju untuk
-      tidak melakukan tindakan berikut: i. Melanggar hak kekayaan intelektual
-      kami atau pihak ketiga. ii. Mengganggu, merusak, atau memanipulasi situs
-      web atau infrastruktur yang terkait. iii. Mengirimkan konten yang
-      melanggar hukum, mengandung materi yang tidak pantas, atau dapat merugikan
-      orang lain. iv. Mengakses data pribadi pengguna lain tanpa izin. Kekayaan
-      Intelektual a. Seluruh konten yang terdapat di situs web ini, termasuk
-      tetapi tidak terbatas pada teks, gambar, grafik, logo, dan merek dagang,
-      dilindungi oleh hukum kekayaan intelektual yang berlaku. b. Anda tidak
-      diizinkan untuk menggunakan, menggandakan, mendistribusikan, atau
-      memanipulasi konten situs web tanpa izin tertulis dari pemiliknya.
-      Penafian Tanggung Jawab a. Situs web ini disediakan "apa adanya" dan kami
-      tidak memberikan jaminan eksplisit atau implisit mengenai keakuratan,
-      keandalan, atau ketersediaan situs web ini. b. Kami tidak bertanggung
-      jawab atas kerugian atau kerusakan yang timbul akibat penggunaan situs web
-      ini atau ketergantungan pada konten yang terdapat di situs web ini.
-      Perubahan Syarat dan Ketentuan Kami berhak untuk mengubah atau memperbarui
-      Syarat dan Ketentuan Layanan ini sesuai kebijakan kami. Perubahan tersebut
-      akan efektif setelah diperbarui di situs web ini. Anda bertanggung jawab
-      untuk secara teratur memeriksa perubahan pada Syarat dan Ketentuan ini.
-      Hukum yang Berlaku Syarat dan Ketentuan ini tunduk pada hukum yang berlaku
-      di [Negara/Provinsi]. Setiap perselisihan yang timbul terkait dengan
-      penggunaan situs web ini akan diselesaikan di pengadilan yang berwenang di
-      [Kota].
-    </p>
+    <>
+      <Navbar />
+      <Layout>
+        <div className="flex items-center justify-center md:max-w-2xl m-auto h-screen">
+          <div className="m-4 text-center">
+            <h1 className="font-semibold text-slate-600 mb-5">
+              TATA TERTIB & KETENTUAN
+            </h1>
+            <Link href="/rules/newclass">
+              <p className="text-slate-600 hover:text-slate-900 border border-orange-600 rounded p-2 mb-2">
+                Cara Membuat Kelas Baru
+              </p>
+            </Link>
+            <Link href="/rules/payment">
+              <p className="text-slate-600 hover:text-slate-900 border border-orange-600 rounded p-2 mb-2">
+                Cara Melakukan Pembayaran
+              </p>
+            </Link>
+            <Link href="/rules/kbm">
+              <p className="text-slate-600 hover:text-slate-900 border border-orange-600 rounded p-2 mb-2">
+                Tata Tertib KBM
+              </p>
+            </Link>
+            <Link href="/rules/odb">
+              <p className="text-slate-600 hover:text-slate-900 border border-orange-600 rounded p-2 mb-2">
+                Apa itu One Day Before
+              </p>
+            </Link>
+            <Link href="/rules/off">
+              <p className="text-slate-600 hover:text-slate-900 border border-orange-600 rounded p-2 mb-2">
+                Off Sementara, Tentu Bisa
+              </p>
+            </Link>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
