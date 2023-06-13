601cfde47080743b77f132c33166fe5254ddf10b
bimbellb3r <bimbellb3r@gmail.com>
2023-06-13 14:37:11 +0800

    coba tanpa redirect ke auth/signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,15 +12,15 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  pages: {
-    signIn: "/auth/signin", //etika user signin diarahkan ke /auth/signin. untuk referensi googling authOption nextauth
-  },
-  callbacks: {
-    async redirect(url, baseUrl) {
-      // Callback untuk mengarahkan pengguna setelah login
-      return "https://bimbellb3r.com";
-    },
-  },
+  // pages: {
+  //   signIn: "/auth/signin", //etika user signin diarahkan ke /auth/signin. untuk referensi googling authOption nextauth
+  // },
+  // callbacks: {
+  //   async redirect(url, baseUrl) {
+  //     // Callback untuk mengarahkan pengguna setelah login
+  //     return "https://bimbellb3r.com";
+  //   },
+  // },
 };
 
 export default NextAuth(authOptions);
