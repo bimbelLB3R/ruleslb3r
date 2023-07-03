f170594ddd9d4029527db6d73c4a58f16da14512
bimbellb3r <bimbellb3r@gmail.com>
2023-07-03 12:13:04 +0800

    styling cek keketatan

--- pages/kampusku/index.js
+++ pages/kampusku/index.js
@@ -6,6 +6,7 @@ import Navbar from "../../components/Navbar";
 import ChartDayaTampung from "../../components/ChartDayaTampung";
 import ChartPeminat from "../../components/ChartPeminat";
 import ChartKeketatan from "../../components/ChartKeketatan";
+import Head from "next/head";
 
 // meminta data ke server /api
 export async function getStaticProps() {
@@ -54,7 +55,7 @@ export default function Kampus({ allKampus, allPost }) {
     values: dataKeketatan,
   };
 
-  console.log(dataKeketatan);
+  // console.log(dataKeketatan);
 
   const handleKampus = (e) => {
     setPilihanKampus(e.target.value);
@@ -68,6 +69,20 @@ export default function Kampus({ allKampus, allPost }) {
 
   return (
     <>
+      <Head>
+        <title>Cek Keketatan Prodi SNBT</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="4x16"
+          href="/image/logolb3r.png"
+        />
+        <meta
+          name="description"
+          content="Cek daya tampung, jumlah peminat dan keketatan prodi jalur SNBT"
+          key="desc"
+        />
+      </Head>
       <Layout>
         <Navbar
           logoUrl="/image/logolb3r.png"
@@ -79,11 +94,27 @@ export default function Kampus({ allKampus, allPost }) {
             <h1 className="mb-4 text-xl font-bold text-orange-900/50">
               Cek Keketatan Prodi
             </h1>
-            <form>
-              <div className="mb-2">
+            <form className="text-gray-600">
+              <div className="mb-2 relative">
+                <div className="absolute  top-1/2 left-1  transform  -translate-y-1/2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+                    />
+                  </svg>
+                </div>
                 <select
                   name="pilihanKampus"
-                  className="w-[300px] border-none "
+                  className="w-[300px] border-none pl-8 bg-transparent"
                   onChange={handleKampus}
                 >
                   <option value="">Pilih Kampus</option>
@@ -98,10 +129,26 @@ export default function Kampus({ allKampus, allPost }) {
                   ))}
                 </select>
               </div>
-              <div className="mb-4">
+              <div className="mb-4 relative">
+                <div className="absolute  top-1/2 left-1  transform  -translate-y-1/2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                    />
+                  </svg>
+                </div>
                 <select
                   name="pilihanProdi"
-                  className="w-[300px] border-none"
+                  className="w-[300px] border-none pl-8 bg-transparent"
                   onChange={handleProdi}
                   // disabled={disableprodi}
                 >
@@ -117,7 +164,7 @@ export default function Kampus({ allKampus, allPost }) {
                   ))}
                 </select>
               </div>
-              <div className="mb-4">
+              <div className="mb-4 shadow-md">
                 <p className="font-semibold text-orange-400">Daya Tampung</p>
                 {pilihanProdi.length === 0 ? (
                   <p className="text-xs text-red-900 italic">Tidak ada data</p>
@@ -134,7 +181,7 @@ export default function Kampus({ allKampus, allPost }) {
                   </div>
                 )}
               </div>
-              <div className="mb-4">
+              <div className="mb-4 shadow-md">
                 <p className="font-semibold text-orange-400">Peminat</p>
                 {pilihanProdi.length === 0 ? (
                   <p className="text-xs text-red-900 italic">Tidak ada data</p>
@@ -151,7 +198,7 @@ export default function Kampus({ allKampus, allPost }) {
                   </div>
                 )}
               </div>
-              <div>
+              <div className="shadow-md">
                 <p className="font-semibold text-orange-400">Keketatan</p>
                 {pilihanProdi.length === 0 ? (
                   <p className="text-xs text-red-900 italic">Tidak ada data</p>
