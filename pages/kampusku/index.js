1b23de5d8eacd58f58d127641b4b09cd11d7941a
bimbellb3r <bimbellb3r@gmail.com>
2023-07-06 14:44:40 +0800

    styling

--- pages/kampusku/index.js
+++ pages/kampusku/index.js
@@ -398,7 +398,7 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                                     <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                                   </svg>
                                 </div>
-                                <p className="text-slate-50 p-2 text-blue-900/70 font-bold">
+                                <p className=" p-2 text-blue-900/70 font-bold">
                                   BELAJAR APA AJA DI PRODI {namaProdi}{" "}
                                 </p>
                               </div>
@@ -461,7 +461,7 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                                     <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
                                   </svg>
                                 </div>
-                                <p className="text-slate-50 p-2 text-yellow-900/70 font-bold">
+                                <p className=" p-2 text-yellow-900/70 font-bold">
                                   PELUANG KARIR PRODI {namaProdi}{" "}
                                 </p>
                               </div>
@@ -523,7 +523,7 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                                     <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8Z" />
                                   </svg>
                                 </div>
-                                <p className="text-slate-50 p-2 text-green-900/70 font-bold">
+                                <p className="p-2 text-green-900/70 font-bold">
                                   STRENGTH TYPOLOGY PRODI {namaProdi}{" "}
                                 </p>
                               </div>
