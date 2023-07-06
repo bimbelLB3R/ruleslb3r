0401c1dcb9a6fe9f663852e1201e56df007e361c
bimbellb3r <bimbellb3r@gmail.com>
2023-07-07 00:05:57 +0800

    mulai nambah matkul

--- pages/kampusku/index.js
+++ pages/kampusku/index.js
@@ -404,28 +404,17 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                               </div>
                             </div>
 
-                            <div className="mt-10">
-                              {item.mata_kuliah?.map((item2, index) => (
-                                <div
-                                  key={index}
-                                  className="flex items-center space-x-2"
-                                >
-                                  <div>
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      width="18"
-                                      height="18"
-                                      fill="currentColor"
-                                      className="bi bi-check2-square"
-                                      viewBox="0 0 16 16"
-                                    >
-                                      <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
-                                      <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
-                                    </svg>
+                            <div className="mt-10 flex flex-wrap">
+                              {item.mata_kuliah
+                                ?.sort((a, b) => a.localeCompare(b))
+                                .map((item2, index) => (
+                                  <div
+                                    key={index}
+                                    className="bg-blue-400/10 p-1 m-1"
+                                  >
+                                    <p>{item2}</p>
                                   </div>
-                                  <p>{item2}</p>
-                                </div>
-                              ))}
+                                ))}
                             </div>
                           </div>
                         ))}
@@ -468,27 +457,29 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                             </div>
 
                             <div className="mt-10">
-                              {item.prospek_kerja?.map((item2, index) => (
-                                <div
-                                  key={index}
-                                  className="flex items-center space-x-2"
-                                >
-                                  <div>
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      width="18"
-                                      height="18"
-                                      fill="currentColor"
-                                      className="bi bi-check2-square"
-                                      viewBox="0 0 16 16"
-                                    >
-                                      <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
-                                      <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
-                                    </svg>
+                              {item.prospek_kerja
+                                ?.sort((a, b) => a.localeCompare(b))
+                                .map((item2, index) => (
+                                  <div
+                                    key={index}
+                                    className="flex items-center space-x-2"
+                                  >
+                                    <div>
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="18"
+                                        height="18"
+                                        fill="currentColor"
+                                        className="bi bi-check2-square"
+                                        viewBox="0 0 16 16"
+                                      >
+                                        <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
+                                        <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                                      </svg>
+                                    </div>
+                                    <p>{item2}</p>
                                   </div>
-                                  <p>{item2}</p>
-                                </div>
-                              ))}
+                                ))}
                             </div>
                           </div>
                         ))}
