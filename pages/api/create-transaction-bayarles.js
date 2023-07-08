fd19f4c73ae17a4f96e2e3c92ff1d46cfb515bbe
bimbellb3r <bimbellb3r@gmail.com>
2023-07-08 22:29:03 +0800

    ubah shandbox dan nambah data

--- pages/api/create-transaction-bayarles.js
+++ pages/api/create-transaction-bayarles.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default async function handler(req, res) {
   const newRow = req.body;
@@ -29,14 +29,14 @@ export default async function handler(req, res) {
   // Please refer to this docs for snap-redirect:
   // https://docs.midtrans.com/en/snap/integration-guide?id=alternative-way-to-display-snap-payment-page-via-redirect
 
-  const snapApiUrl = 'https://app.midtrans.com/snap/v1/transactions';
-  const snapServerKey = 'Mid-server-zzOiNRXtsXKskJUV-kAyWdD1';
+  const snapApiUrl = "https://app.midtrans.com/snap/v1/transactions";
+  const snapServerKey = "Mid-server-zzOiNRXtsXKskJUV-kAyWdD1";
   const timestamp = Date.now();
 
   // Prepare request body
   // satu order_id hanya untuk satu kali request
   const requestBody = {
-    payment_type: 'qris', //sy tambahkan ini, moga dapat url qris
+    payment_type: "qris", //sy tambahkan ini, moga dapat url qris
     transaction_details: {
       order_id: `LB3R_${timestamp}`,
       gross_amount: totalDibayar, //quantity x price
@@ -56,9 +56,9 @@ export default async function handler(req, res) {
     // },
 
     // gopay, berhasil
-    enabled_payments: ['gopay'],
+    enabled_payments: ["gopay"],
     gopay: {
-      callback_url: 'http://gopay.com',
+      callback_url: "http://gopay.com",
     },
 
     item_details: [
@@ -71,56 +71,56 @@ export default async function handler(req, res) {
     ],
     customer_details: {
       first_name: newRow.namalengkap,
-      last_name: 'LB3R',
-      email: 'bimbellb3r@gmail.com',
+      last_name: "LB3R",
+      email: "bimbellb3r@gmail.com",
       phone: newRow.wa,
       billing_address: {
         first_name: newRow.namalengkap,
-        last_name: 'LB3R',
-        email: 'bimbellb3r@gmail.com',
+        last_name: "LB3R",
+        email: "bimbellb3r@gmail.com",
         phone: newRow.wa,
-        address: 'Tabalong',
-        city: 'Tabalong',
-        postal_code: '71571',
-        country_code: 'IDN',
+        address: "Tabalong",
+        city: "Tabalong",
+        postal_code: "71571",
+        country_code: "IDN",
       },
       shipping_address: {
         first_name: newRow.namalengkap,
-        last_name: 'LB3R',
-        email: 'bimbellb3r@gmail.com',
+        last_name: "LB3R",
+        email: "bimbellb3r@gmail.com",
         phone: newRow.wa,
-        address: 'Tabalong',
-        city: 'Tabalong',
-        postal_code: '71571',
-        country_code: 'IDN',
+        address: "Tabalong",
+        city: "Tabalong",
+        postal_code: "71571",
+        country_code: "IDN",
       },
     },
   };
 
   // Set request headers
   const headers = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    Authorization: `Basic ${Buffer.from(snapServerKey).toString('base64')}`,
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Basic ${Buffer.from(snapServerKey).toString("base64")}`,
   };
 
   try {
     const response = await axios.post(snapApiUrl, requestBody, { headers });
     const transactionToken = response.data.token;
-    console.log('transactionToken:', transactionToken);
+    console.log("transactionToken:", transactionToken);
 
     // transaction redirect url
     const transactionRedirectUrl = response.data.redirect_url;
-    console.log('transactionRedirectUrl:', transactionRedirectUrl);
+    console.log("transactionRedirectUrl:", transactionRedirectUrl);
 
     // const qrisUrl = response.data.qris_url;
     // console.log('qrisUrl:', qrisUrl);
 
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json({ transactionToken, transactionRedirectUrl });
   } catch (error) {
-    console.log('Error occurred:', error.message);
-    res.setHeader('Content-Type', 'application/json');
-    res.status(500).json({ error: 'Failed to create transaction' });
+    console.log("Error occurred:", error.message);
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).json({ error: "Failed to create transaction" });
   }
 }
--- pages/api/create-transaction.js
+++ pages/api/create-transaction.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default async function handler(req, res) {
   const newRow = req.body;
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
   // Please refer to this docs for snap-redirect:
   // https://docs.midtrans.com/en/snap/integration-guide?id=alternative-way-to-display-snap-payment-page-via-redirect
 
-  const snapApiUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions';
-  const snapServerKey = 'SB-Mid-server-EdNalvrIc1PCrcDoVVOnpRV2';
+  const snapApiUrl = "https://app.midtrans.com/snap/v1/transactions";
+  const snapServerKey = "Mid-server-zzOiNRXtsXKskJUV-kAyWdD1";
   const timestamp = Date.now();
 
   // Prepare request body
@@ -42,48 +42,48 @@ export default async function handler(req, res) {
       phone: newRow.wa,
       billing_address: {
         first_name: newRow.nama,
-        last_name: 'LB3R',
+        last_name: "LB3R",
         email: newRow.email,
         phone: newRow.wa,
         address: newRow.asalsekolah,
-        city: 'Tabalong',
-        postal_code: '71571',
-        country_code: 'IDN',
+        city: "Tabalong",
+        postal_code: "71571",
+        country_code: "IDN",
       },
       shipping_address: {
         first_name: newRow.nama,
-        last_name: 'LB3R',
+        last_name: "LB3R",
         email: newRow.email,
         phone: newRow.wa,
         address: newRow.asalsekolah,
-        city: 'Tabalong',
-        postal_code: '71571',
-        country_code: 'IDN',
+        city: "Tabalong",
+        postal_code: "71571",
+        country_code: "IDN",
       },
     },
   };
 
   // Set request headers
   const headers = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    Authorization: `Basic ${Buffer.from(snapServerKey).toString('base64')}`,
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Basic ${Buffer.from(snapServerKey).toString("base64")}`,
   };
 
   try {
     const response = await axios.post(snapApiUrl, requestBody, { headers });
     const transactionToken = response.data.token;
-    console.log('transactionToken:', transactionToken);
+    console.log("transactionToken:", transactionToken);
 
     // transaction redirect url
     const transactionRedirectUrl = response.data.redirect_url;
-    console.log('transactionRedirectUrl:', transactionRedirectUrl);
+    console.log("transactionRedirectUrl:", transactionRedirectUrl);
 
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json({ transactionToken, transactionRedirectUrl });
   } catch (error) {
-    console.log('Error occurred:', error.message);
-    res.setHeader('Content-Type', 'application/json');
-    res.status(500).json({ error: 'Failed to create transaction' });
+    console.log("Error occurred:", error.message);
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).json({ error: "Failed to create transaction" });
   }
 }
