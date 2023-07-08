503e7ed13ed8bf9c17072b222ace14ab6965f1a8
bimbellb3r <bimbellb3r@gmail.com>
2023-07-09 00:20:39 +0800

    styling

--- pages/layanan/berhasil.js
+++ pages/layanan/berhasil.js
@@ -3,7 +3,7 @@ export default function Success() {
   const router = useRouter();
   const { order_id } = router.query;
   return (
-    <>
+    <div className="flex items-center justify-center h-screen">
       <div className="border-4 p-2 text-lg">
         <p>Pembayaran Sukses</p>
         <p>ID Pemesanan = {order_id}</p>
@@ -11,6 +11,6 @@ export default function Success() {
           (Tunjukkan ID Pemesanan ke Admin sebagai bukti pembayaran)
         </p>
       </div>
-    </>
+    </div>
   );
 }
