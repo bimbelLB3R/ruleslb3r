edafbc9a4a0ab02f301dea109693ea68b4f044e2
bimbellb3r <bimbellb3r@gmail.com>
2023-07-09 09:13:39 +0800

    ubah axios ke fetch

--- pages/layanan/notification.js
+++ pages/layanan/notification.js
@@ -1,32 +1,19 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 export default function NotificationPage() {
   const [notificationData, setNotificationData] = useState(null);
-  console.log(notificationData);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post("/api/layanan/notification"); // Ganti dengan URL endpoint notifikasi yang sesuai
-        setNotificationData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
-    fetchData();
+  useEffect(() => {
+    fetch("/api/layanan/notification") // Ganti dengan URL endpoint notifikasi yang sesuai
+      .then((response) => response.json())
+      .then((data) => setNotificationData(data))
+      .catch((error) => console.error(error));
   }, []);
 
   return (
     <div>
       {notificationData ? (
-        <div>
-          <h1>Notification Details</h1>
-          <p>Transaction Type: {notificationData.transaction_type}</p>
-          <p>Transaction Time: {notificationData.transaction_time}</p>
-          <p>Transaction Status: {notificationData.transaction_status}</p>
-          {/* Tambahkan lebih banyak pemetaan sesuai dengan kebutuhan Anda */}
-        </div>
+        <pre>{JSON.stringify(notificationData, null, 2)}</pre>
       ) : (
         <p>Loading...</p>
       )}
