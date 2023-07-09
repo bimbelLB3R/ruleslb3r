f857bb2c557b41ddfcc2bc53ddd4555c62d3af14
bimbellb3r <bimbellb3r@gmail.com>
2023-07-09 09:31:00 +0800

    ubah api

--- pages/api/layanan/notification.js
+++ pages/api/layanan/notification.js
@@ -1,13 +1,13 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const data = req.body; // Mendapatkan data yang dikirimkan oleh Midtrans
-    // Lakukan pemrosesan data sesuai kebutuhan Anda
-    // Misalnya, Anda dapat mengakses nilai-nilai dengan data.transaction_type, data.transaction_time, dsb.
-    console.log(data);
+  const data = req.body; // Mendapatkan data yang dikirimkan oleh Midtrans
+  // Lakukan pemrosesan data sesuai kebutuhan Anda
+  // Misalnya, Anda dapat mengakses nilai-nilai dengan data.transaction_type, data.transaction_time, dsb.
+  // console.log(data);
 
-    // Mengirimkan respons dengan status HTTP 200
+  // Mengirimkan respons dengan status HTTP 200
+  if (data) {
     res.status(200).json({ data: data });
   } else {
-    res.status(405).end(); // Mengirimkan respons dengan status HTTP 405 jika metode selain POST diakses
+    res.status("Tidak ada data");
   }
 }
