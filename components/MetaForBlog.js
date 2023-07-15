dd96011625cbdf41b76d8b6f62e94ff9c077bd0d
bimbellb3r <bimbellb3r@gmail.com>
2023-07-15 08:42:08 +0800

    meta tag

--- components/MetaForBlog.js
+++ components/MetaForBlog.js
@@ -13,6 +13,21 @@ export default function MetaForBlog({ detailPost }) {
       <meta property="og:title" content={ogTitle} />
       <meta property="og:description" content={ogDesc} />
       <meta property="og:url" content={process.env.PUBLIC_URL} />
+      <meta property="og:type" content="website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="author" content="Wahyudi" />
+      <meta name="keywords" content={detailPost.keyword} />
+      <meta name="robots" content="index,follow" />
+
+      {/* Telegram meta tags */}
+      <meta property="article:author" content="Wahyudi" />
+      <meta property="article:section" content={detailPost.tags} />
+      <meta property="article:tag" content={detailPost.keyword} />
+
+      <meta name="twitter:card" content={ogImage} />
+      <meta name="twitter:title" content={ogTitle} />
+      <meta name="twitter:description" content={ogDesc} />
+      <meta name="twitter:image" content={ogImage} />
     </Head>
   );
 }
