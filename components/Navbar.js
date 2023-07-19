a5d07a779f1a1f64ffcf4c27a8489788e86cf090
bimbellb3r <bimbellb3r@gmail.com>
2023-07-19 08:13:56 +0800

    styling navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -142,7 +142,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
                   </div>
                 </Link>
                 <Link href="/layanan">
-                  <div className="bg-orange-600 text-slate-50 px-6 py-4 text-center mb-2 rounded">
+                  <div className="bg-orange-600 p-1 text-slate-50 m-1 rounded">
                     <div className="flex items-center space-x-2">
                       <div>
                         <svg
@@ -165,7 +165,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
                 </Link>
                 {/* <DropdownMenuLayanan /> */}
                 <Link href="/blogs">
-                  <div className="bg-orange-600 text-slate-50 px-6 py-4 text-center mb-2 rounded">
+                  <div className="bg-orange-600 p-1 text-slate-50 m-1 rounded">
                     <div className="flex items-center space-x-2">
                       <div>
                         <svg
@@ -188,7 +188,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
 
                 <div>
                   <Link href="/form/newmember">
-                    <div className="bg-orange-600 text-slate-50 px-6 py-4 text-center mb-2 rounded">
+                    <div className="bg-orange-600 p-1 text-slate-50 m-1 rounded">
                       <div className="flex items-center space-x-2">
                         <div>
                           <svg
@@ -209,7 +209,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
                     </div>
                   </Link>
                   <Link href="/form/login">
-                    <div className="bg-orange-600 text-slate-50 px-6 py-4 text-center mb-2 rounded">
+                    <div className="bg-orange-600 p-1 text-slate-50 m-1 rounded">
                       <div className="flex items-center space-x-2">
                         <div>
                           <svg
@@ -235,7 +235,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
                     </div>
                   </Link>
                   <Link href="/kampusku">
-                    <div className="bg-orange-600 text-slate-50 px-6 py-4 text-center mb-2 rounded">
+                    <div className="bg-orange-600 p-1 text-slate-50 m-1 rounded">
                       <div className="flex items-center space-x-2">
                         <div>
                           <svg
@@ -247,7 +247,7 @@ const Navbar = forwardRef(({ allPost, ...props }, ref) => {
                             <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
                           </svg>
                         </div>
-                        <p>CEK KEKETATAN STUDI</p>
+                        <p>CEK KEKETATAN PRODI</p>
                       </div>
                     </div>
                   </Link>
