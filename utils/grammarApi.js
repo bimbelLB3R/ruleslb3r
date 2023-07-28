360e4918134903694181da1bbfc85f78be83ac8b
bimbellb3r <bimbellb3r@gmail.com>
2023-07-28 21:08:52 +0800

    api sendiri,json sendiri

--- utils/grammarApi.js
+++ utils/grammarApi.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 export function getGrammarData() {
-  const filePath = "./pages/api/english.json";
+  const filePath = "./pages/api/grammar.json";
   const jsonData = fs.readFileSync(filePath, "utf8");
   const data = JSON.parse(jsonData);
   return data;
