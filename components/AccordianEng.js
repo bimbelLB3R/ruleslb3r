9a6435c0fb00509f71d1f98e35c4b49d85006c3d
bimbellb3r <bimbellb3r@gmail.com>
2023-07-31 09:25:05 +0800

    speaking

--- components/AccordianEng.js
+++ components/AccordianEng.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 const AccordianEng = ({ allEnglish }) => {
   const allGrammar = allEnglish.filter((item) => item.theme === "grammar");
   const allPro = allEnglish.filter((item) => item.theme === "pronaunciation");
+  const allSpeak = allEnglish.filter((item) => item.theme === "speaking");
   return (
     <div className="flex items-center justify-center mt-10">
       <div className="md:max-w-2xl mb-10 overflow-hidden">
@@ -106,9 +107,46 @@ const AccordianEng = ({ allEnglish }) => {
               </p>
             </Accordion.Title>
             <Accordion.Content>
-              <p className="mb-2 text-gray-900 dark:text-gray-400">
-                Dalam Proses
-              </p>
+              <div>
+                {allSpeak.map((item, index) => (
+                  <div key={index}>
+                    <Link
+                      href={`/pare/${item.slug}`}
+                      className="flex items-center space-x-2 border-b-2 m-3"
+                    >
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                          />
+                        </svg>
+                      </div>
+                      <div>
+                        <p>
+                          {item.title}-
+                          <span className="text-xs italic">
+                            {item.description}
+                          </span>
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </Accordion.Content>
           </Accordion.Panel>
           <Accordion.Panel>
