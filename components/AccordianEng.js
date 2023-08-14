eb5d3feaa001b0d206a0b546a9a0f94219ec5603
bimbellb3r <bimbellb3r@gmail.com>
2023-08-14 11:22:29 +0800

    mtk sma selesai up load

--- components/AccordianEng.js
+++ components/AccordianEng.js
@@ -1,176 +1,72 @@
 import { Accordion } from "flowbite-react";
 import Link from "next/link";
 const AccordianEng = ({ allEnglish }) => {
-  const allGrammar = allEnglish.filter((item) => item.theme === "grammar");
-  const allPro = allEnglish.filter((item) => item.theme === "pronaunciation");
-  const allSpeak = allEnglish.filter((item) => item.theme === "speaking");
+  const data = [
+    {
+      judul: "Pronaunciation (Pengucapan Huruf-Kata)",
+      namaMap: allEnglish.filter((item) => item.theme === "pronaunciation"),
+    },
+    {
+      judul: "Grammar (Aturan Tata Bahasa)",
+      namaMap: allEnglish.filter((item) => item.theme === "grammar"),
+    },
+    {
+      judul: "Speaking (Berbicara Bahasa Inggris)",
+      namaMap: allEnglish.filter((item) => item.theme === "speaking"),
+    },
+  ];
+
   return (
     <div className="flex items-center justify-center mt-10">
       <div className="md:max-w-2xl mb-10 overflow-hidden">
         <Accordion alwaysOpen={true}>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">
-                Pronaunciation (Pengucapan Huruf-Kata)
-              </p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allPro.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/pare/${item.slug}`,
-                        query: { theme: "pronaunciation", id: item.id },
-                      }}
-                      // href={`/pare/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>{item.title}</div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Grammar (Aturan Tata Bahasa)</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allGrammar.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/pare/${item.slug}`,
-                        query: { theme: "grammar", id: item.id },
-                      }}
-                      // href={`/pare/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>{item.title}</div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Vocabularry (Kosa-kata)</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-900 dark:text-gray-400">
-                Dalam proses
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">
-                Speaking (Berbicara Bahasa Inggris)
-              </p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allSpeak.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/pare/${item.slug}`,
-                        query: { theme: "speaking", id: item.id },
-                      }}
-                      // href={`/pare/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>
-                        <p>
-                          {item.title}-
-                          <span className="text-xs italic">
-                            {item.description}
-                          </span>
-                        </p>
-                      </div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Kumpulan E-book</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-900 dark:text-gray-400 text-left">
-                Dalam Proses
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
+          {data.map((itemi, index) => (
+            <Accordion.Panel key={index}>
+              <Accordion.Title>
+                <p className="text-orange-900">{itemi.judul}</p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div>
+                  {itemi.namaMap.map((item, index) => (
+                    <div key={index}>
+                      <Link
+                        href={{
+                          pathname: `/pare/${item.slug}`,
+                          query: { theme: item.theme, id: item.id },
+                        }}
+                        passHref
+                        // href={`/math/${item.slug}`}
+                        className="flex items-center space-x-2 border-b-2 m-3"
+                      >
+                        <div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                            />
+                          </svg>
+                        </div>
+                        <div>{item.title}</div>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
+          ))}
         </Accordion>
       </div>
     </div>
--- components/AccordianMath.js
+++ components/AccordianMath.js
@@ -3,148 +3,99 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 const AccordianMath = ({ allMath }) => {
-  const router = useRouter();
-  const allIntegral = allMath.filter((item) => item.bab === "Integral");
-  const allLimit = allMath.filter((item) => item.bab === "Limit dan Turunan");
-  const allBangunRuang = allMath.filter(
-    (item) => item.bab === "Analisis Bangun Ruang"
-  );
+  const data = [
+    {
+      judul: "Integral",
+      namaMap: allMath.filter((item) => item.bab === "Integral"),
+    },
+    {
+      judul: "Limit dan Turunan",
+      namaMap: allMath.filter((item) => item.bab === "Limit dan Turunan"),
+    },
+
+    {
+      judul: "Analisis Bangun Ruang",
+      namaMap: allMath.filter((item) => item.bab === "Analisis Bangun Ruang"),
+    },
+    {
+      judul: "Aturan Sinus dan Kosinus",
+      namaMap: allMath.filter(
+        (item) => item.bab === "Aturan Sinus dan Kosinus"
+      ),
+    },
+    {
+      judul: "Barisan dan Deret",
+      namaMap: allMath.filter((item) => item.bab === "Barisan dan Deret"),
+    },
+    {
+      judul: "Fungsi Kuadrat",
+      namaMap: allMath.filter((item) => item.bab === "Fungsi Kuadrat"),
+    },
+    {
+      judul: "Komposisi Fungsi",
+      namaMap: allMath.filter((item) => item.bab === "Komposisi Fungsi"),
+    },
+    {
+      judul: "Logaritma",
+      namaMap: allMath.filter((item) => item.bab === "Logaritma"),
+    },
+    {
+      judul: "Matriks",
+      namaMap: allMath.filter((item) => item.bab === "Matriks"),
+    },
+  ];
+
   return (
     <div className="flex items-center justify-center mt-10">
       <div className="md:max-w-2xl mb-10 overflow-hidden">
         <Accordion alwaysOpen={true}>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Integral</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allIntegral.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/math/${item.slug}`,
-                        query: { bab: "Integral", id: item.id },
-                      }}
-                      // href={`/math/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>{item.title}</div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Limit dan Turunan</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allLimit.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/math/${item.slug}`,
-                        query: { bab: "Limit dan Turunan", id: item.id },
-                      }}
-                      // href={`/math/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>{item.title}</div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>
-              <p className="text-orange-900">Analisis Bangun Ruang</p>
-            </Accordion.Title>
-            <Accordion.Content>
-              <div>
-                {allBangunRuang.map((item, index) => (
-                  <div key={index}>
-                    <Link
-                      href={{
-                        pathname: `/math/${item.slug}`,
-                        query: { bab: "Analisis Bangun Ruang", id: item.id },
-                      }}
-                      // href={`/math/${item.slug}`}
-                      className="flex items-center space-x-2 border-b-2 m-3"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                          />
-                        </svg>
-                      </div>
-                      <div>{item.title}</div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
+          {data.map((itemi, index) => (
+            <Accordion.Panel key={index}>
+              <Accordion.Title>
+                <p className="text-orange-900">{itemi.judul}</p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div>
+                  {itemi.namaMap.map((item, index) => (
+                    <div key={index}>
+                      <Link
+                        href={{
+                          pathname: `/math/${item.slug}`,
+                          query: { bab: item.bab, id: item.id },
+                        }}
+                        passHref
+                        // href={`/math/${item.slug}`}
+                        className="flex items-center space-x-2 border-b-2 m-3"
+                      >
+                        <div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                            />
+                          </svg>
+                        </div>
+                        <div>{item.title}</div>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
+          ))}
         </Accordion>
       </div>
     </div>
