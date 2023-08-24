0da6a89f983474056732b677a148aadf9f58de89
bimbellb3r <bimbellb3r@gmail.com>
2023-08-24 10:38:44 +0800

    lagi checkbox

--- components/Newmember.js
+++ components/Newmember.js
@@ -418,6 +418,7 @@ export default function Newmember() {
                   <div className="flex items-center h-5">
                     <input
                       id="terms"
+                      name="cekaja"
                       aria-describedby="terms"
                       type="checkbox"
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
