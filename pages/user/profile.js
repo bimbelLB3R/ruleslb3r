3e973c329c5ccac7be4b27a379bc0af7e79aa827
bimbellb3r <bimbellb3r@gmail.com>
2023-08-31 13:08:28 +0800

    styling 2

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -26,29 +26,41 @@ export default function Profile({ allPost }) {
       <Layout>
         <div className="flex justify-center items-center p-6 mt-20">
           {session ? (
-            <div>
-              <div className="flex justify-center">
-                <Image
-                  src={session.user.image}
-                  width={46}
-                  height={46}
-                  alt="userFoto"
-                  priority={true}
-                  className="rounded-full shadow-2xl"
-                />
-              </div>
-              <p className="text-center">{session.user.name}</p>
-              <p className="text-center mb-4">{session.user.email}</p>
-              <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                Pembayaran Tertunda : 0
-              </div>
-              <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                Pembayaran Berhasil : 0
-              </div>
-              <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                <p className="font-semibold">Pilihan Jurusan</p>
-                <p>1. Pilihan Pertama</p>
-                <p>2. Pilihan Kedua</p>
+            <div className="overflow-auto">
+              <div>
+                <div className="flex justify-center">
+                  <Image
+                    src={session.user.image}
+                    width={46}
+                    height={46}
+                    alt="userFoto"
+                    priority={true}
+                    className="rounded-full shadow-2xl"
+                  />
+                </div>
+                <p className="text-center">{session.user.name}</p>
+                <p className="text-center mb-4">{session.user.email}</p>
+                <div className="font-semibold p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                  Pembayaran Tertunda : 0
+                </div>
+                <div className="font-semibold p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                  Pembayaran Berhasil : 0
+                </div>
+                <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                  <p className="font-semibold">Pilihan Jurusan</p>
+                  <p>1. Pilihan Pertama</p>
+                  <p>2. Pilihan Kedua</p>
+                </div>
+                <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                  <p className="font-semibold">Skor Rata-rata TO SNBT</p>
+                  <p>1. Literasi BI : -</p>
+                  <p>2. Literasi English : -</p>
+                  <p>3. Penalaran Umum : -</p>
+                  <p>4. Pemahaman Bacaan & Menulis : -</p>
+                  <p>5. Penalaran Matematika : -</p>
+                  <p>6. Pengetahuan & Pemahaman Umum : -</p>
+                  <p>7. Pengetahuan Kuantitatif : -</p>
+                </div>
               </div>
             </div>
           ) : (
