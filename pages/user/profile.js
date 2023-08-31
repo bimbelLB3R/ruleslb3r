22a4e78b1073479cca2666c8bbbf64c15cefc8c3
bimbellb3r <bimbellb3r@gmail.com>
2023-08-31 12:24:22 +0800

    err solve

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 import { getBlogsData } from "../../utils/blogsApi";
 import { getTutorialData } from "../../utils/TutorialApi";
 import "animate.css";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export async function getStaticProps() {
@@ -24,28 +24,41 @@ export default function Profile({ allPost }) {
     <div>
       <Navbar allPost={allPost} />
       <Layout>
-        <div className="flex justify-center p-6">
-          <div>
-            <div className="border-2 border-white rounded-full ">
-              <div className="w-[12px] h-[12px]  rounded-full bg-green-400"></div>
-              <Image
-                src={session.user.image}
-                width={46}
-                height={46}
-                alt="userFoto"
-                priority={true}
-                className="rounded-full shadow-2xl"
-              />
+        <div className="flex justify-center items-center p-6">
+          {session ? (
+            <div>
+              <div className="border-2 border-white rounded-full ">
+                <div className="w-[12px] h-[12px]  rounded-full bg-green-400"></div>
+                <Image
+                  src={session.user.image}
+                  width={46}
+                  height={46}
+                  alt="userFoto"
+                  priority={true}
+                  className="rounded-full shadow-2xl"
+                />
+              </div>
+              <p>{session.user.name}</p>
+              <p>{session.user.email}</p>
+              <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                Pembayaran Tertunda : 0
+              </div>
+              <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
+                Pembayaran Berhasil : 0
+              </div>
             </div>
-            <p>{session.user.name}</p>
-            <p>{session.user.email}</p>
-            <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-              Pembayaran Tertunda : 0
+          ) : (
+            <div className="flex justify-center items-center p-6">
+              <button
+                type="submit"
+                name="loginGoogle"
+                onClick={() => signIn()}
+                className="underline"
+              >
+                Anda Belum Login
+              </button>
             </div>
-            <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-              Pembayaran Berhasil : 0
-            </div>
-          </div>
+          )}
         </div>
       </Layout>
     </div>
