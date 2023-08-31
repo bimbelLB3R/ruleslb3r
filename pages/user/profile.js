f792d87cded02832e7c39f7056a82452c1cb6b76
bimbellb3r <bimbellb3r@gmail.com>
2023-08-31 21:01:58 +0800

    styling profile

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -22,68 +22,87 @@ export default function Profile({ biodata }) {
   return (
     <div>
       {/* <Navbar allPost={allPost} /> */}
-      <Layout>
-        <div className="flex justify-center items-center p-6 mt-20">
-          {session ? (
-            <div className="overflow-auto">
-              <div>
-                <div className="flex justify-center">
-                  <Image
-                    src={session.user.image}
-                    width={46}
-                    height={46}
-                    alt="userFoto"
-                    priority={true}
-                    className="rounded-full shadow-2xl"
-                  />
-                </div>
-                <p className="text-center">{session.user.name}</p>
-                <p className="text-center mb-4">{session.user.email}</p>
-                <div className="font-semibold p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                  Pembayaran Tertunda : 0
-                </div>
-                <div className="font-semibold p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                  Pembayaran Berhasil : 0
-                </div>
-                {choosenEmail.map((item, index) => (
-                  <div key={index}>
-                    <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                      <p className="font-semibold">Pilihan Jurusan</p>
-                      <p>
-                        1. Pilihan Pertama : {item[4]}, {item[6]}
-                      </p>
-                      <p>
-                        2. Pilihan Kedua : {item[5]}, {item[7]}
-                      </p>
+
+      <div className="flex justify-center items-center p-6 mt-20">
+        {session ? (
+          <div className="overflow-auto">
+            <div>
+              <div className="flex justify-center">
+                <Image
+                  src={session.user.image}
+                  width={66}
+                  height={66}
+                  alt="userFoto"
+                  priority={true}
+                  className="rounded-full shadow-2xl"
+                />
+              </div>
+              <p className="text-center">{session.user.name}</p>
+              <p className="text-center mb-4">{session.user.email}</p>
+              <div className="font-semibold p-2 bg-blue-200 shadow-sm shadow-amber-600 m-2 rounded-lg">
+                Pembayaran Tertunda : 0
+              </div>
+              <div className="font-semibold p-2 bg-blue-200 shadow-sm shadow-amber-600 m-2 rounded-lg">
+                Pembayaran Berhasil : 0
+              </div>
+              {choosenEmail.map((item, index) => (
+                <div key={index}>
+                  <div className="p-2 bg-blue-200 shadow-sm shadow-amber-600 m-2 rounded-lg">
+                    <p className="font-semibold">Pilihan Jurusan</p>
+                    <p>
+                      1. Pilihan Pertama : {item[4]}, {item[6]}
+                    </p>
+                    <p>
+                      2. Pilihan Kedua : {item[5]}, {item[7]}
+                    </p>
+                  </div>
+                  <div className="p-2 bg-blue-200 shadow-sm shadow-amber-600 m-2 rounded-lg flex flex-wrap md:max-w-xl">
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[11]}</p>
+                      <p className="text-sm">LBI</p>
                     </div>
-                    <div className="p-2 bg-blue-200 shadow-lg shadow-amber-600 m-2 rounded-lg">
-                      <p className="font-semibold">Skor Rata-rata TO SNBT</p>
-                      <p>1. Literasi BI : {item[11]}</p>
-                      <p>2. Literasi English : {item[14]}</p>
-                      <p>3. Penalaran Umum : {item[16]}</p>
-                      <p>4. Pemahaman Bacaan & Menulis : {item[15]}</p>
-                      <p>5. Penalaran Matematika : {item[13]}</p>
-                      <p>6. Pengetahuan & Pemahaman Umum : {item[17]}</p>
-                      <p>7. Pengetahuan Kuantitatif : {item[12]}</p>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[14]}</p>
+                      <p className="text-sm">LBE</p>
+                    </div>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[16]}</p>
+                      <p className="text-sm">PU</p>
+                    </div>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[15]}</p>
+                      <p className="text-sm">PBM</p>
+                    </div>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[13]}</p>
+                      <p className="text-sm">PM</p>
+                    </div>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[17]}</p>
+                      <p className="text-sm">PPU</p>
+                    </div>
+                    <div className="text-center bg-slate-300 rounded p-1 m-1">
+                      <p className="text-xl text-red-900">{item[12]}</p>
+                      <p className="text-sm">PK</p>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-          ) : (
-            <div className="flex justify-center items-center p-6 mt-20">
-              <button
-                type="submit"
-                name="loginGoogle"
-                onClick={() => signIn()}
-                className="underline"
-              >
-                Anda Belum Login
-              </button>
-            </div>
-          )}
-        </div>
-      </Layout>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center p-6 mt-20">
+            <button
+              type="submit"
+              name="loginGoogle"
+              onClick={() => signIn()}
+              className="underline"
+            >
+              Anda Belum Login
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
