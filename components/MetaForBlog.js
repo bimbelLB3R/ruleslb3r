0c0c8566a22a24035f9b186fa0fb7d1260c218c8
bimbellb3r <bimbellb3r@gmail.com>
2023-09-07 15:52:16 +0800

    tampilan og di fb

--- components/MetaForBlog.js
+++ components/MetaForBlog.js
@@ -9,15 +9,15 @@ export default function MetaForBlog({ detailPost }) {
   return (
     <Head>
       <title>{ogTitle}</title>
-      <meta property="og:image" itemProp="image" content={ogImage} />
+      <meta property="og:image" content={ogImage} />
       <meta property="og:title" content={ogTitle} />
       <meta property="og:description" content={ogDesc} />
-      <meta property="og:url" content={process.env.PUBLIC_URL} />
-      <meta property="og:type" content="website" />
+      {/* <meta property="og:url" content={process.env.PUBLIC_URL} /> */}
+      {/* <meta property="og:type" content="website" /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="author" content="Wahyudi" />
       <meta name="keywords" content={detailPost.keyword} />
-      <meta name="robots" content="index,follow" />
+      {/* <meta name="robots" content="index,follow" /> */}
 
       {/* Telegram meta tags */}
       <meta property="article:author" content="Wahyudi" />
