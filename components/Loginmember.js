dfeb2093ea91f41ca9c4ce8957b7ab0f309ed85d
bimbellb3r <bimbellb3r@gmail.com>
2024-02-26 13:48:18 +0800

    foto user di login SNBT

--- components/Loginmember.js
+++ components/Loginmember.js
@@ -186,8 +186,24 @@ const Loginmember = () => {
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              {session ? (
+                <div className="flex items-center justify-center m-4">
+                  <div className="border-2 border-white rounded-full relative">
+                    <Image
+                      src={session.user.image}
+                      width={36}
+                      height={36}
+                      alt="userFoto"
+                      priority={true}
+                      className="rounded-full shadow-2xl"
+                    />
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Simulasi UTBK SNBT
               </h1>
               <form
                 className="space-y-4 md:space-y-6"
