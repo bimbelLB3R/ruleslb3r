558af49c4568f47b467fac3a2b9820bca4a23c94
bimbellb3r <bimbellb3r@gmail.com>
2024-03-10 00:14:35 +0800

    setting feedback

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,67 +1,168 @@
 import Image from "next/image";
 import StarRating from "../../components/RatingTentor";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+// Config variables
+const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID_RATING;
+const SHEET_ID1 = process.env.NEXT_PUBLIC_SHEET_ID_RATING1;
+const SHEET_ID2 = process.env.NEXT_PUBLIC_SHEET_ID_RATING2;
+const GOOGLE_CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
+const GOOGLE_SERVICE_PRIVATE_KEY =
+  process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
 
 const FeedbackForm = () => {
+  const handleSignIn = async () => {
+    try {
+      // Menetapkan prompt ke 'select_account' saat memanggil signIn
+      await signIn("google", { prompt: "select_account" });
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
+  const { data: session } = useSession();
+
+  const [dataJadwal, setDataJadwal] = useState([]);
+
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  const ambilJadwal = async (email_user) => {
+    await doc.useServiceAccountAuth({
+      client_email: GOOGLE_CLIENT_EMAIL,
+      private_key: GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    await doc.loadInfo(); // tambahkan baris ini untuk memastikan sheet telah terdefinisi
+    const sheet1 = doc.sheetsById[SHEET_ID1]; // tambahkan baris ini untuk mendefinisikan sheet
+    const sheet2 = doc.sheetsById[SHEET_ID2]; // tambahkan baris ini untuk mendefinisikan sheet
+    const rows = await sheet1.getRows(); //data jadwal
+    const rows2 = await sheet2.getRows(); //data siswa
+    // setDataJadwal(jadwalSesuaiEmail);
+    // setDataSiswa(rows2);
+    // console.log(email_user);
+
+    const cekEmailUser = rows2.find(
+      (row) => row.email_user === `${email_user}`
+    ); //penulisan row.name , name nya harus sama dengan di google sheet name
+    const kelasUser = cekEmailUser.kelas_user;
+    const jadwalSesuaiKelasUser = rows.filter(
+      (row) => row.kelas_jadwal === `${kelasUser}`
+    );
+    // setKelasUserState(kelasUser);
+    console.log(jadwalSesuaiKelasUser);
+    setDataJadwal(jadwalSesuaiKelasUser);
+  };
+  useEffect(() => {
+    // Panggil fungsi ambilJadwal disini
+
+    if (session) {
+      const userMail = session.user.email;
+      ambilJadwal(userMail);
+    }
+  }, []);
+
   const [ratings, setRatings] = useState(0);
   const [submitted, setSubmitted] = useState({});
 
   // Contoh data orang
-  const people = [
-    { id: 1, name: "Paijo" },
-    { id: 2, name: "Inem" },
-    { id: 3, name: "Cenil" },
-  ];
+  // const people = [
+  //   { id: 1, name: "Paijo" },
+  //   { id: 2, name: "Inem" },
+  //   { id: 3, name: "Cenil" },
+  // ];
 
-  const handleRatingChange = (personId, value) => {
-    const personName = people.find((person) => person.id === personId).name;
-    if (!submitted[personId]) {
-      setRatings({ ...ratings, [personId]: value });
+  const handleRatingChange = (jadwalId, value) => {
+    const jadwalName = dataJadwal.find(
+      (jadwal) => jadwal.id_jadwal === jadwalId
+    ).pengajar_jadwal;
+    if (!submitted[jadwalId]) {
+      setRatings({ ...ratings, [jadwalId]: value });
     }
   };
 
-  const handleSubmit = (personId) => {
-    const personName = people.find((person) => person.id === personId).name;
+  const handleSubmit = (jadwalId) => {
+    const jadwalName = dataJadwal.find(
+      (jadwal) => jadwal.id_jadwal === jadwalId
+    ).pengajar_jadwal;
     // Validasi apakah pengguna memberikan penilaian sebelum mengirimkan data
-    if (ratings[personId] !== undefined) {
+    if (ratings[jadwalId] !== undefined) {
       // Kirim rating ke server atau lakukan tindakan lainnya.
-      console.log(`${personName}'s Rating:`, ratings[personId]);
-      setSubmitted({ ...submitted, [personId]: true });
+      console.log(`${jadwalName}'s Rating:`, ratings[jadwalId]);
+      setSubmitted({ ...submitted, [jadwalId]: true });
     } else {
-      alert(`Anda belum memberikan penilaian untuk ${personName}`);
+      alert(`Anda belum memberikan penilaian untuk ${jadwalName}`);
     }
   };
 
   return (
     <div className=" mt-20">
-      <h2 className="text-center font-bold uppercase">Beri Penilaian</h2>
-      {people.map((person) => (
-        <form
-          key={person.id}
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(person.id);
-          }}
-        >
-          <div className="flex items-center justify-center space-x-3">
-            <Image
-              src="/image/image1.webp"
-              width={100}
-              height={100}
-              alt="Pengajar"
-              className="rounded-full"
-              priority
-            />
-            <StarRating
-              onChange={(value) => handleRatingChange(person.id, value)}
-              disabled={submitted[person.id]}
-            />
-            <button type="submit" disabled={submitted[person.id]}>
-              {submitted[person.id] ? "Terkirim" : "Kirim"}
+      <Head>
+        <title>Rating Pengajar | Bimbel LB3R</title>
+        <meta
+          name="description"
+          content="Rating terhadap perfoma guru saat mengajar"
+          key="desc"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="4x16"
+          href="image/logolb3r.png"
+        />
+      </Head>
+      {session ? (
+        <div>
+          <h2 className="text-center font-bold uppercase">Beri Penilaian</h2>
+          {dataJadwal.map((daJal) => (
+            <form
+              key={daJal.id_jadwal}
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit(daJal.id_jadwal);
+              }}
+            >
+              <div className="flex items-center justify-center space-x-3 border-b-2 border-gray-300 p-2 max-w-[300px] md:max-w-xl m-auto">
+                <div>
+                  <Image
+                    src="/image/image1.webp"
+                    width={100}
+                    height={100}
+                    alt="Pengajar"
+                    className="rounded-full"
+                    priority
+                  />
+                  <p className="text-center">{daJal.pengajar_jadwal}</p>
+                </div>
+
+                <StarRating
+                  onChange={(value) =>
+                    handleRatingChange(daJal.id_jadwal, value)
+                  }
+                  disabled={submitted[daJal.id_jadwal]}
+                />
+                <button type="submit" disabled={submitted[daJal.id_jadwal]}>
+                  {submitted[daJal.id_jadwal] ? "Terkirim" : "Kirim"}
+                </button>
+              </div>
+            </form>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <div className="flex items-center space-x-2 justify-center font-roboto mt-4 ">
+            <button
+              type="submit"
+              name="loginGoogle"
+              // onClick={() => signIn()}
+              onClick={handleSignIn}
+              className="underline  bg-slate-900   text-slate-50 px-3 py-3 rounded-xl"
+            >
+              Login dengan Google
             </button>
           </div>
-        </form>
-      ))}
+        </div>
+      )}
     </div>
   );
 };
