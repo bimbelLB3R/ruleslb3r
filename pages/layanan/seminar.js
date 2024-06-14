f8ae91a4a2ed80a3ac0ec761538381c2d71076f3
bimbelLB3R <bimbellb3r@gmail.com>
2024-06-14 16:27:44 +0800

    buat state untuk peserta umum

--- pages/layanan/seminar.js
+++ pages/layanan/seminar.js
@@ -31,6 +31,7 @@ const Seminar = ({allSeminar}) => {
   const [iskelasEmpty, setIskelasEmpty] = useState(false);
   const [ismasalahEmpty, setIsmasalahEmpty] = useState(false);
   const [iskuliahEmpty, setIskuliahEmpty] = useState(false);
+  const [selectedClass, setSelectedClass] = useState(""); // State to manage selected class
   const router = useRouter();
   const [form, setForm] = useState({
     wa: "",
@@ -115,6 +116,9 @@ const Seminar = ({allSeminar}) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
+    if (name === "kelas") {
+      setSelectedClass(value); // Update the selected class
+    }
   };
 
   return (
@@ -158,6 +162,18 @@ const Seminar = ({allSeminar}) => {
               FORMULIR PENDAFTARAN
             </p>
             <div>
+            <select
+                className={`w-full mb-2 px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 ${iskelasEmpty ? "border-red-500" : ""}`}
+                name="kelas"
+                onBlur={() => setIskelasEmpty(!form.kelas)}
+                onChange={handleChange}
+              >
+                <option value="">Pilih Kelas</option>
+                <option value="X SMA">X SMA</option>
+                <option value="XI SMA">XI SMA</option>
+                <option value="Umum">Umum</option>
+              </select>
+              {iskelasEmpty && <p className="text-red-500 text-xs mb-2">Wajib diisi</p>}
               <div className="relative">
                 <input
                   name="wa"
@@ -170,7 +186,7 @@ const Seminar = ({allSeminar}) => {
                 <label
                   className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-slate-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                 >
-                  Nomor WA Orang tua Siswa
+                  {selectedClass === "Umum" ? "Nomor WA" : "Nomor WA Orang tua Siswa"}
                 </label>
               </div>
               {iswaEmpty && <p className="text-red-500 text-xs mb-2">Wajib diisi</p>}
@@ -187,26 +203,16 @@ const Seminar = ({allSeminar}) => {
                 <label
                   className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-slate-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                 >
-                  Nama Lengkap Anak
+                 {selectedClass === "Umum" ? "Nama Lengkap" : "Nama Lengkap Anak"}
                 </label>
               </div>
               {isanakEmpty && <p className="text-red-500 text-xs mb-2">Wajib diisi</p>}
 
-              <select
-                className={`w-full mb-2 px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 ${iskelasEmpty ? "border-red-500" : ""}`}
-                name="kelas"
-                onBlur={() => setIskelasEmpty(!form.kelas)}
-                onChange={handleChange}
-              >
-                <option value="">Pilih Kelas</option>
-                <option value="X SMA">X SMA</option>
-                <option value="XI SMA">XI SMA</option>
-              </select>
-              {iskelasEmpty && <p className="text-red-500 text-xs mb-2">Wajib diisi</p>}
+              
 
               <textarea
                 className={`w-full px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 mb-2 ${ismasalahEmpty ? "border-red-500" : ""}`}
-                placeholder="Kendala yang dihadapi orang tua dengan anak"
+                placeholder={selectedClass === "Umum" ? "Alasan Ikut Seminar" : "Kendala yang dihadapi orang tua dengan anak"}
                 name="masalah"
                 onChange={handleChange}
               ></textarea>
@@ -214,7 +220,7 @@ const Seminar = ({allSeminar}) => {
 
               <textarea
                 className={`w-full px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 mb-2 ${iskuliahEmpty ? "border-red-500" : ""}`}
-                placeholder="Rencana kuliah jurusan apa dan kampus mana"
+                placeholder={selectedClass === "Umum" ? "Alamat Domisili" : "Rencana kuliah jurusan apa dan kampus mana"}
                 name="kuliah"
                 onChange={handleChange}
               ></textarea>
