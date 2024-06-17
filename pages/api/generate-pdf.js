bbdb08e27f4f1a0da0c1c98a4cae9fd8ad54e9ed
bimbelLB3R <bimbellb3r@gmail.com>
2024-06-17 23:23:47 +0800

    ubah puppeteer ke core dan isntal aws chrome

--- pages/api/generate-pdf.js
+++ pages/api/generate-pdf.js
@@ -1,30 +1,34 @@
-// pages/api/generate-pdf.js
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
 
 export default async (req, res) => {
-    const { type } = req.query;
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    //   jika ingin menampilkan pesan eror, tambahkan
-    // headless: false, // Ini akan menampilkan browser untuk debugging
-  });
-  const page = await browser.newPage();
-//   jika ingin menampilkan pesan eror, tambahkan
-//   page.on('console', msg => console.log('PAGE LOG:', msg.text())); // Log pesan dari halaman
-  
-  try {
-    await page.goto(`https://www.bimbellb3r.com/blogs/${type}`, { waitUntil: 'networkidle0', timeout: 60000 });
-    // menambahkan ini agar komponen yang diberi class no-print tidak tampil di pdf
-    await page.addStyleTag({ content: '@media print {.no-print { display: none; }}' });
-    // jika data yang mau di pdf bersifat dinamis, tambahkan
-    // await page.waitForSelector('#content'); // Ganti dengan selector elemen yang ingin ditunggu, kurang efektif untuk data yang ngambil dr spreadsheet.
+  const { type } = req.query;
+  let browser = null;
 
-    const pdf = await page.pdf({ format: 'A4',margin: {
+  try {
+    // Launch browser using chrome-aws-lambda
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(`https://www.bimbellb3r.com/blogs/${type}`, {
+      waitUntil: 'networkidle0',
+      timeout: 60000,
+    });
+    await page.addStyleTag({ content: '@media print {.no-print { display: none; }}' });
+
+    const pdf = await page.pdf({
+      format: 'A4',
+      margin: {
         top: '3cm',
         right: '3cm',
         bottom: '3cm',
-        left: '3cm'
-      } });
+        left: '3cm',
+      },
+    });
 
     await browser.close();
 
@@ -32,7 +36,7 @@ export default async (req, res) => {
     res.setHeader('Content-Disposition', 'attachment; filename=halaman.pdf');
     res.send(pdf);
   } catch (error) {
-    await browser.close();
+    if (browser) await browser.close();
     res.status(500).send('Error generating PDF: ' + error.message);
   }
 };
