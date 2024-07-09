8642d78d2bba20acb4eac5275205f221490a6df2
bimbelLB3R <bimbellb3r@gmail.com>
2024-07-09 11:14:26 +0800

    mengubah tinggi hal pdf

--- pages/layanan/diagnostik/hasil.js
+++ pages/layanan/diagnostik/hasil.js
@@ -4,7 +4,6 @@ import ChartDiagnostik from "../../../components/ChartDiagnostik";
 import Swal from "sweetalert2";
 import { getModalitasData } from "../../../utils/modalitasApi";
 import Head from "next/head";
-import DownloadPdf from "../../../components/DownloadPdf";
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
@@ -114,31 +113,37 @@ const hasilDiagnostik = ({ allTipeModalitas }) => {
 
   const handleDownload = async () => {
     const element = document.getElementById('page-content');
-    const canvas = await html2canvas(element);
+    const canvas = await html2canvas(element, { scale: 2 }); // Meningkatkan skala untuk kualitas yang lebih baik
     const data = canvas.toDataURL('image/png');
 
-    // const pdf = new jsPDF();
-    // const imgProperties = pdf.getImageProperties(data);
-    // const pdfWidth = pdf.internal.pageSize.getWidth();
-    // const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
-
-    // Ukuran halaman A4 dalam satuan mm
-    const pdf = new jsPDF('p', 'mm', 'a4');
+    // Ukuran halaman kustom dalam satuan mm
+    const pdf = new jsPDF('p', 'mm', [210, 400]);
     const pdfWidth = 210;
-    const pdfHeight = 297;
+    const pdfHeight = 400;
     
-    // Rasio ukuran gambar untuk menyesuaikan dengan halaman A4
+    // Rasio ukuran gambar untuk menyesuaikan dengan halaman PDF
     const imgWidth = pdfWidth;
     const imgHeight = (canvas.height * pdfWidth) / canvas.width;
 
-    // pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    pdf.addImage(data, 'PNG', 0, 0, imgWidth, imgHeight);
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(data, 'PNG', 0, position, imgWidth, imgHeight);
+    heightLeft -= pdfHeight;
+
+    while (heightLeft > 0) {
+      position -= pdfHeight;
+      pdf.addPage();
+      pdf.addImage(data, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pdfHeight;
+    }
+
     pdf.save(`Modalitas-${namaAnak}.pdf`);
   };
 
   return (
     <>
-    <Head>
+      <Head>
         <title>HASIL DIAGNOSTIK| Bimbel LB3R</title>
         <meta
           name="description"
@@ -152,40 +157,40 @@ const hasilDiagnostik = ({ allTipeModalitas }) => {
           href="/image/logolb3r.png"
         />
       </Head>
-    <div className="flex items-center justify-center m-4 " >
-      <div>
-      <div className="md:max-w-3xl print:w-[794px] print:h-[1123px] border p-4 md:p-10 shadow-md" id="page-content">
-        <div className="flex items-center justify-center bg-gray-900 p-2 m-2">
-          <p className="text-gray-50 font-semibold">HASIL TES MODALITAS BELAJAR</p>
-        </div>
-        <div className="p-2">
-          <div className="mb-2 flex space-x-2"> 
-            <p>Nama :</p>
-            <p>{namaAnak}</p>
+      <div className="flex items-center justify-center m-4">
+        <div>
+          <div className="md:max-w-3xl print:w-[794px] print:h-[1123px] border p-4 md:p-10 shadow-md" id="page-content">
+            <div className="flex items-center justify-center bg-gray-900 p-2 m-2">
+              <p className="text-gray-50 font-semibold">HASIL TES MODALITAS BELAJAR</p>
+            </div>
+            <div className="p-2">
+              <div className="mb-2 flex space-x-2"> 
+                <p>Nama :</p>
+                <p>{namaAnak}</p>
+              </div>
+              <div className="mb-2 flex space-x-2">
+                <p>Hasil Diagnostik :</p>
+                <p className="font-semibold">{result}</p>
+              </div>
+              <div className="w-[300px] md:w-[600px] mb-2">
+                <ChartDiagnostik data={data} />
+              </div>
+              <div className="mb-2">
+                <p>Keterangan :</p>
+                <p>{keterangan}</p>
+              </div>
+            </div>
           </div>
-          <div className="mb-2 flex space-x-2">
-            <p>Hasil Diagnostik :</p>
-            <p className="font-semibold">{result}</p>
-          </div>
-          <div className="w-[300px] md:w-[600px] mb-2">
-            <ChartDiagnostik data={data} />
-          </div>
-          <div className="mb-2">
-            <p>Keterangan :</p>
-            <p>{keterangan}</p>
+          <div>
+            <div className="flex items-center justify-center bg-gray-600 p-2 m-2">
+              <button onClick={handleDownload} className="text-gray-50 font-semibold">Download Hasil</button>
+            </div>
+            <div className="flex items-center justify-center bg-red-600 p-2 m-2">
+              <button onClick={handleTutup} className="text-gray-50 font-semibold">Keluar</button>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-      <div className="flex items-center justify-center bg-gray-600 p-2 m-2">
-          <button onClick={handleDownload} className="text-gray-50 font-semibold">Download Hasil</button>
-        </div>
-        <div className="flex items-center justify-center bg-red-600 p-2 m-2">
-          <button onClick={handleTutup} className="text-gray-50 font-semibold">Keluar</button>
-        </div>
-        </div>
-        </div>
-    </div>
     </>
   );
 };
