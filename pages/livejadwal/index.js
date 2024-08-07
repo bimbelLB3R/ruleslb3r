05186bc2cd371acfb4062599edff8cb16927d0ad
bimbelLB3R <bimbellb3r@gmail.com>
2024-08-07 11:52:39 +0800

    perbaiki kode

--- pages/livejadwal/index.js
+++ pages/livejadwal/index.js
@@ -15,6 +15,7 @@ const GOOGLE_SERVICE_PRIVATE_KEY = process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVAT
 const LiveJadwal = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [dataJadwal, setDataJadwal] = useState([]);
+  
 
   const ambilJadwal = async () => {
     try {
@@ -47,6 +48,7 @@ const LiveJadwal = () => {
   useEffect(() => {
     ambilJadwal();
   }, []);
+  
 
   return (
     <Layout>
@@ -66,18 +68,21 @@ const LiveJadwal = () => {
             <div className="mb-20">
               {dataJadwal.length > 0 ? (
                 <div>
-                  {dataJadwal.map((daJal) => (
+                  {dataJadwal.map((daJal) => {
+                    const srcGambar= `image/tentor/${daJal.pengajar_jadwal}.png`;
                     <div key={daJal.id_jadwal}>
                       <div className="flex items-center justify-start space-x-3 border-b-2 border-gray-300 p-2 max-w-[300px] md:max-w-xl m-auto">
                         <div>
-                          <Link href={`https://www.bimbellb3r.com/image/tentor/${daJal.pengajar_jadwal}.png`} className="flex items-center justify-center">
+                          <Link href={`https://www.bimbellb3r.com/${srcGambar}`} className="flex items-center justify-center">
                             <Image
-                              src={`/image/tentor/${daJal.pengajar_jadwal.length>0?daJal.pengajar_jadwal:'iconlb3r'}.png`}
+                              // src={`/image/tentor/${daJal.pengajar_jadwal}.png`}
+                              src={`/${srcGambar}`}
                               width={50}
                               height={50}
                               alt="No Image"
                               className="rounded-full"
                               priority
+                              onError={(e) => e.target.src = "image/tentor/iconlb3r.png"}
                             />
                           </Link>
                           
@@ -96,7 +101,7 @@ const LiveJadwal = () => {
                         </div>
                       </div>
                     </div>
-                  ))}
+})}
                 </div>
               ) : (
                 <p className="text-center p-3">Jadwal belum tersedia</p>
