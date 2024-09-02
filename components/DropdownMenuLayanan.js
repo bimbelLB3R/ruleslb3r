41402cb499775cd0ee8a90d2445a610ce02d3452
bimbelLB3R <bimbellb3r@gmail.com>
2024-09-02 17:23:46 +0800

    ST-30

--- components/DropdownMenuLayanan.js
+++ components/DropdownMenuLayanan.js
@@ -86,7 +86,7 @@ export default function DropdownMenuLayanan() {
               Cek Keketatan Prodi
             </Link>
             <Link
-              href="https://www.jambore.temubakat.com"
+              href="https://temubakat.com/id/"
               className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-200"
               role="menuitem"
               tabIndex="-1"
--- pages/kampusku/index.js
+++ pages/kampusku/index.js
@@ -498,8 +498,6 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                       </div>
                     )}
                   </div>
-                </div>
-                <div className="md:w-1/2">
                   <div
                     className={`${
                       pilihanProdi.length === 0 || defProdi.length === 0
@@ -543,6 +541,8 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                       ""
                     )}
                   </div>
+                </div>
+                <div className="md:w-1/2">
 
                   <div
                     className={`${
@@ -691,7 +691,7 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                               </div>
                             </div>
 
-                            <div className="mt-[60px]">
+                            <div className="mt-[40px]">
                               {item.st30?.map((item2, index) => (
                                 <div
                                   key={index}
@@ -714,16 +714,17 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                                       </p>
                                     </div>
                                   ))}
-                                  {item2.putih?.map((item3, index) => (
+                                  {/* {item2.putih?.map((item3, index) => (
                                     <div key={index} className="mb-2 border-2">
                                       <p className=" p-1 bg-gray-100/50 font-roboto text-sm">
                                         {item3}
                                       </p>
                                     </div>
-                                  ))}
+                                  ))} */}
                                 </div>
                               ))}
                             </div>
+                            
                             <div>
                               <a
                                 href="https://temubakat.com/id/index.php/main/disp/tes"
@@ -746,6 +747,48 @@ export default function Kampus({ allKampus, allPost, definisiProdi }) {
                                   />
                                 </svg>
                               </a>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <div
+                    className={`${
+                      pilihanProdi.length === 0 || defProdi.length === 0
+                        ? ""
+                        : "shadow-md p-3 text-sm  relative"
+                    }`}
+                  >
+                    {pilihanProdi.length !== 0 ? (
+                      <div className="pt-6 ">
+                        {defProdi.map((item, index) => (
+                          <div key={index} className=" p-2 relative">
+                            <div className="absolute  transform   -translate-y-1/2 bg-orange-400/20">
+                              <div className="p-2 flex items-center">
+                                <div className="">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="36"
+                                    height="36"
+                                    fill="currentColor"
+                                    className="bi bi-question-diamond-fill text-orange-900/70"
+                                    viewBox="0 0 16 16"
+                                  >
+                                    <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM5.495 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z" />
+                                  </svg>
+                                </div>
+                                <p className=" p-2 text-orange-900/70 font-bold">
+                                  PENJELASAN ST-30 untuk  {namaProdi}{" "}
+                                </p>
+                              </div>
+                            </div>
+                            <div className="mt-10">
+                              <p>
+                                {item.kriteria}
+                              </p>
                             </div>
                           </div>
                         ))}
