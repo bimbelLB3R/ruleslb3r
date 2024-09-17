613accab05459229c8b9c711fbb631f32b7a7628
bimbelLB3R <bimbellb3r@gmail.com>
2024-09-17 11:14:00 +0800

    ubah tampilan layanan

--- pages/layanan/index.js
+++ pages/layanan/index.js
@@ -5,9 +5,11 @@ import Wa from "../../components/Wa";
 import { getProgramsData } from "../../utils/layananApi";
 import "animate.css";
 import Head from "next/head";
-
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { getBlogsData } from "../../utils/blogsApi";
 import { getTutorialData } from "../../utils/TutorialApi";
+import Image from "next/image";
 // mengubah mata uang
 function formatCurrency(amount) {
   const formatter = new Intl.NumberFormat("id-ID", {
@@ -59,64 +61,46 @@ export default function Layanan({ allProgram, allPost, allTutorial }) {
         />
       </Head>
       <Wa />
-      <Navbar logoUrl="/image/logolb3r.png" logoAlt="Logo" allPost={allPost} />
+      {/* <Navbar logoUrl="/image/logolb3r.png" logoAlt="Logo" allPost={allPost} /> */}
       <Layout>
-        {/* <div className="flex items-center justify-center">
-          <p className="mt-20 font-bold text-[20px] text-blue-600 uppercase mb-10">
-            Program Bimbel LB3R
-          </p>
-        </div> */}
-
-        <div className="flex items-center justify-center mt-[100px] font-roboto">
-          <div className="md:flex md:flex-wrap md:justify-center">
+      <div className="flex items-center justify-center bg-gradient-to-b from-purple-800  to-purple-500 md:p-[100px]">
+      <div className="md:max-w-2xl  overflow-hidden">
+        <Carousel showArrows={true}
+            showThumbs={false}
+            autoPlay={false}
+            infiniteLoop={true}>
             {allProgram.map((program, index) => (
               <div
                 key={index}
-                className={`card bg-gradient-to-b from-purple-600 drop-shadow-lg m-4 p-5 border border-slate-300 md:w-[400px] w-[320px] ${
+                className={`card bg-gradient-to-b from-purple-600 drop-shadow-lg p-5   ${
                   (program.keterangan === "Best Seller" &&
                     "bg-gradient-to-b from-amber-500/50") ||
                   (program.keterangan === "Free/Gratis" &&
                     "bg-gradient-to-b from-gray-500/50")
                 }`}
               >
-                {program.keterangan === "Best Seller" ? (
-                  <div className="animate__animated  animate__slideInDown absolute rounded-b-2xl shadow-lg right-0 top-0 p-2 bg-amber-500 text-xl text-gray-900 font-bold">
-                    Best Seller
-                  </div>
-                ) : (
-                  ""
-                )}
-                {program.keterangan === "Populer" ? (
-                  <div className="animate__animated  animate__slideInDown absolute rounded-b-2xl shadow-lg right-0 top-0 p-2 bg-purple-500 text-xl text-gray-900 font-bold">
-                    Populer
-                  </div>
-                ) : (
-                  ""
-                )}
-                {program.keterangan === "Free/Gratis" ? (
-                  <div className="animate__animated  animate__slideInDown absolute rounded-b-2xl shadow-lg right-0 top-0 p-2 bg-gray-500 text-xl text-gray-900 font-bold">
-                    Free/Gratis
-                  </div>
-                ) : (
-                  ""
-                )}
-                <div className="">
-                  <p className="w-2/3 font-semibold text-slate-800 tracking-wider underline underline-offset-4 decoration-4 decoration-orange-600 ">
-                    {program.nama}
-                  </p>
-                </div>
-                <div className="flex items-center justify-center">
+                <Image src="/image/image9.png"
+                alt="Foto siswa"
+                width={500}
+                height={500}
+                priority={true}
+                className="object-scale-down sm:object-cover"/>
+                
+                <div className="flex items-center justify-center -mt-10">
                   <div>
-                    <p className="text-[40px] md:text-[30px] font-bold mt-10 text-orange-400">
+                    <p className="text-[20px] md:text-[40px] font-bold text-orange-900">
+                      {program.nama}
+                    </p>
+                    <p className="text-[10px] md:text-[20px] font-bold text-orange-400">
                       {formatCurrency(program.biaya)}
                     </p>
                     <p className="text-center">{program.frekuensi}</p>
                   </div>
                 </div>
-                <ul className="leading-normal  p-3 text-slate-600">
+                <ul className="leading-normal  p-3 text-slate-200">
                   {program.fasilitas.map((elemen, index) => (
-                    <li className="flex items-start space-x-3" key={index}>
-                      <svg
+                    <li className="flex items-center justify-center space-x-3" key={index}>
+                      {/* <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
                         height="16"
@@ -125,8 +109,8 @@ export default function Layanan({ allProgram, allPost, allTutorial }) {
                         viewBox="0 0 16 16"
                       >
                         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                      </svg>
-                      <p>{elemen}</p>
+                      </svg> */}
+                      <p className="text-[12px] md:text-sm bg-purple-800">{elemen}</p>
                     </li>
                   ))}
                 </ul>
@@ -136,8 +120,9 @@ export default function Layanan({ allProgram, allPost, allTutorial }) {
                       ? `/form/newmember`
                       : `/layanan/${program.slug}`
                   }
+                  className="flex items-center justify-center"
                 >
-                  <div className="bg-gradient-to-b from-yellow-400 to-white  text-purple-800  mt-5 flex items-center space-x-3 justify-center rounded-full mb-10">
+                  <div className="bg-gradient-to-b from-yellow-400 to-white  text-purple-800 mt-5 flex items-center space-x-3 justify-center rounded-full mb-5 md:mb-10 md:w-1/2">
                     <p className="text-center font-semibold px-2 py-4 ">
                       DAFTAR SEKARANG
                     </p>
@@ -160,8 +145,9 @@ export default function Layanan({ allProgram, allPost, allTutorial }) {
                 </Link>
               </div>
             ))}
-          </div>
-        </div>
+         </Carousel>
+         </div>
+         </div>
       </Layout>
     </>
   );
