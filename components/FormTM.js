af2d7b198c7373b378c6d826524185c0ee9ae1fc
bimbelLB3R <bimbellb3r@gmail.com>
2024-09-27 16:30:18 +0800

    Layanan TM:Styling

--- components/FormTM.js
+++ components/FormTM.js
@@ -604,7 +604,7 @@ const FormTM = ({ detailProgram}) => {
                 name="profesi2"
                 type="text"
                 id="floating_outlined6"
-                className={`mb-2  block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                className={`mb-2  block px-2.5 pb-2.5 pt-8 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
                   isProfesi2Empty ? "border-red-500" : "mb-2"
                 }`}
                 placeholder=" "
