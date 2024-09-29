23b8d645ea6595e028865bd4a37af8f640d6bc3a
bimbelLB3R <bimbellb3r@gmail.com>
2024-09-29 23:21:30 +0800

    Styling agar footer tetap dibawah

--- components/FormTM.js
+++ components/FormTM.js
@@ -458,7 +458,7 @@ const FormTM = ({ detailProgram}) => {
               <input
                 name="email"
                 type="email"
-                required
+                // required
                 id="floating_outlined1"
                 className="mb-2  block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer "
                 placeholder=" "
@@ -479,7 +479,7 @@ const FormTM = ({ detailProgram}) => {
               <input
                 name="nama"
                 type="text"
-                required
+                // required
                 id="floating_outlined2"
                 className="mb-2  block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,8 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="font-roboto">
-      {children}
+    <div className="font-roboto flex flex-col min-h-screen">
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
