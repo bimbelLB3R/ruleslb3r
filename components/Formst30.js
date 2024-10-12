1576dedc963f05deaa2660532e5fc0127fce2374
bimbelLB3R <bimbellb3r@gmail.com>
2024-10-12 09:30:46 +0800

    Tambah fitur Tipologi

--- components/Formst30.js
+++ components/Formst30.js
@@ -1,11 +1,13 @@
 import { Button, Table, TextInput } from 'flowbite-react';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export default function Formst30() {
-    const [codes, setCodes] = useState('');
+    const [inputValue, setInputValue] = useState('');
     const [sortedData, setSortedData] = useState([]);
     const [sortedTalents, setSortedTalents] = useState([]);
+    const [sortedKelompok, setSortedKelompok] = useState([]);
     const [tooltip, setTooltip] = useState({ show: false, text: '' });
+    const [colors, setColors] = useState([]);
     const [activityDescriptions, setActivityDescriptions] = useState({}); // State for activity descriptions
     const [activeActivity, setActiveActivity] = useState(null); // Track which activity is active
 
@@ -20,39 +22,103 @@ export default function Formst30() {
         // Set the tooltip text and make sure to clear the tooltip if it already shows
         setTooltip({ show: true, text: data[activity] || 'Description not available' });
     };
-    console.log(tooltip)
+    // console.log(tooltip)
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // Pisahkan input berdasarkan spasi
+    const inputs = inputValue.split(' ');
+
+    const codesArray = [];
+    const talentsArray = [];
+
+    // Memisahkan codes dan talents
+    inputs.forEach(item => {
+        if (item.startsWith('#')) {
+            // Jika diawali dengan '#', maka dianggap sebagai talent
+            talentsArray.push(item.substring(1)); // Hapus tanda '#' di depan
+        } else {
+            // Selain itu, dianggap sebagai code
+            codesArray.push(item);
+        }
+    });
         const response = await fetch('/api/st30/getSortedTipologi', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ codes: codes.split(' ') }),
+            body: JSON.stringify({ codes: codesArray, talents: talentsArray }),
         });
         const data = await response.json();
         setSortedData(data.sortedData);
         setSortedTalents(data.sortedTalents);
+        setSortedKelompok(data.kelompokPercentages);
     };
+    // console.log(sortedCategory)
+    // Fungsi untuk menghasilkan warna acak dalam format hex
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+    // Set warna acak untuk setiap kategori saat komponen pertama kali di-render
+    useEffect(() => {
+        const randomColors = sortedKelompok.map(() => getRandomColor());
+        setColors(randomColors);
+    }, [sortedKelompok]);
 
     return (
         <div>
+            <div className="w-full mt-0">
+                <div className="flex">
+                    <div className="relative w-full h-8 bg-gray-200">
+                        {sortedKelompok.map(([kelompok, percentage], index) => (
+                            <div
+                                key={index}
+                                className="absolute h-full text-center"
+                                style={{
+                                    width: `${percentage}%`, // Sesuaikan lebar berdasarkan persentase
+                                    left: `${sortedKelompok.slice(0, index).reduce((acc, curr) => acc + parseFloat(curr[1]), 0)}%`, // Hitung posisi left
+                                    backgroundColor: colors[index], // Gunakan warna acak
+                                }}
+                            >
+                                <span className="text-white text-xs p-1 ">{kelompok} ({percentage}%)</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
             <div className="flex items-center justify-center m-10">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className='relative'>
                     <div className="flex items-center justify-center space-x-3">
-                        <TextInput
-                            type="text"
-                            value={codes}
-                            onChange={(e) => setCodes(e.target.value)}
-                            placeholder="Enter codes e.g. MED DES CRE"
-                        />
+                        <div className="relative">
+                            <TextInput
+                                type="text"
+                                value={inputValue}
+                                className='w-[300px] md:w-[500px]'
+                                onChange={(e) => setInputValue(e.target.value)}
+                                placeholder="Enter codes or talents (#Talent) separated by spaces"
+                            />
+                            {inputValue && (
+                                <button
+                                    type="button"
+                                    className="absolute top-1/2 transform -translate-y-1/2 right-3 text-gray-500"
+                                    onClick={() => setInputValue('')}
+                                >
+                                    ✕
+                                </button>
+                            )}
+                        </div>
                         <Button gradientDuoTone="pinkToOrange" type="submit">
                             Submit
                         </Button>
                     </div>
                 </form>
             </div>
-            <div className="flex items-center justify-center max-w-3xl p-4 m-auto">
+
+            <div className="flex items-center justify-center max-w-3xl p-4 m-auto mb-20">
                 <Table>
                     <Table.Head>
                         <Table.HeadCell>Code</Table.HeadCell>
@@ -98,20 +164,20 @@ export default function Formst30() {
                 </Table>
             </div>
             {tooltip.show && (
-                <div className="fixed bottom-0 p-2 bg-gray-900 text-gray-100 left-0 right-0">
+                <div className="fixed z-50 bottom-0 p-2 bg-gray-900 text-gray-100 left-0 right-0">
                     <p className="text-center text-xs">{tooltip.text}</p>
                 </div>
             )}
             <div className="flex items-center justify-center max-w-3xl p-4 m-auto">
                 <Table>
                     <Table.Head>
-                        <Table.HeadCell>Urutan Bakat</Table.HeadCell>
+                        <Table.HeadCell>Persentase Kelompok</Table.HeadCell>
                     </Table.Head>
                     <Table.Body className="divide-y">
-                        {sortedTalents.map((item, index) => (
+                        {sortedKelompok.map((item, index) => (
                             <Table.Row key={index} className="bg-white dark:bg-gray-800">
-                                <Table.Cell>
-                                    {item}
+                                <Table.Cell className='flex justify-center items-center space-x-2'>
+                                    <p>{item[0]}</p><p>{item[1]}%</p>
                                 </Table.Cell>
                             </Table.Row>
                         ))}
--- pages/api/st30/getSortedTipologi.js
+++ pages/api/st30/getSortedTipologi.js
@@ -2,30 +2,67 @@ import tipologi from './tipologi.json';
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        const { codes } = req.body;
+        const { codes,talents:userTalents } = req.body;
 
         const sortedData = [];
         const talentCount = {};
+        const kelompokCount = {}; // Untuk menghitung frekuensi kategori
 
-        // Mengambil data berdasarkan urutan input user
-        codes.forEach(code => {
-            if (tipologi[code]) {
-                const { activities, roles, talents, category } = tipologi[code];
-                sortedData.push({ code, activities, roles, talents, category });
+         // Jika 'codes' dikirim, lakukan pencarian berdasarkan 'codes'
+         if (codes && codes.length > 0) {
+            codes.forEach(code => {
+                if (tipologi[code]) {
+                    const { activities, roles, talents, category,kelompok } = tipologi[code];
+                    sortedData.push({ code, activities, roles, talents, category,kelompok });
 
-                // Hitung frekuensi setiap talent
-                talents.forEach(talent => {
-                    talentCount[talent] = (talentCount[talent] || 0) + 1;
-                });
-            }
-        });
+                    // Hitung frekuensi setiap talent yang ditemukan dari 'codes'
+                    talents.forEach(talent => {
+                        talentCount[talent] = (talentCount[talent] || 0) + 1;
+                    });
+
+                    // Hitung frekuensi setiap kategori
+                kelompokCount[kelompok] = (kelompokCount[kelompok] || 0) + 1;
+                }
+                
+            });
+            
+        }
+
+        // Jika 'userTalents' dikirim, lakukan pencarian berdasarkan 'talents'
+        if (userTalents && userTalents.length > 0) {
+            Object.keys(tipologi).forEach(code => {
+                const { activities, roles, talents, category,kelompok } = tipologi[code];
+
+                // Cek apakah talents dari user ada di talents tipologi ini
+                const matchingTalents = talents.filter(talent => userTalents.includes(talent));
+
+                if (matchingTalents.length > 0) {
+                    sortedData.push({ code, activities, roles, talents, category,kelompok });
+
+                    // Hitung frekuensi setiap talent yang cocok
+                    matchingTalents.forEach(talent => {
+                        talentCount[talent] = (talentCount[talent] || 0) + 1;
+                    });
+                    // Hitung frekuensi setiap kategori
+                kelompokCount[kelompok] = (kelompokCount[kelompok] || 0) + 1;
+                }
+                
+            });
+        }
 
         // Urutkan talents berdasarkan frekuensi kemunculan
         const sortedTalents = Object.entries(talentCount)
             .sort((a, b) => b[1] - a[1]) // Urutkan dari yang paling banyak muncul
             .map(([talent]) => talent); // Ambil hanya nama talent
+        // Hitung persentase setiap kategori
+        const totalItems = sortedData.length;
+        const kelompokPercentages = Object.keys(kelompokCount).map(kelompok => {
+            const count = kelompokCount[kelompok];
+            const percentage = ((count / totalItems) * 100).toFixed(2); // Hitung persentase dan batasi hingga 2 angka desimal
+            return [ kelompok, percentage ];
+        });
 
-        res.status(200).json({ sortedData, sortedTalents });
+        res.status(200).json({ sortedData, sortedTalents,kelompokPercentages });
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
