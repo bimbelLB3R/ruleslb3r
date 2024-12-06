13a5845f842a82df12f151c1b98ceb1840ed843a
bimbelLB3R <bimbellb3r@gmail.com>
2024-12-07 06:16:59 +0800

    link pengajar

--- components/DropdownMenuLayanan.js
+++ components/DropdownMenuLayanan.js
@@ -95,7 +95,7 @@ export default function DropdownMenuLayanan() {
               Assesment Talents Mapping
             </Link>
             <Link
-              href="#"
+              href="/pengajar"
               className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-200"
               role="menuitem"
               tabIndex="-1"
