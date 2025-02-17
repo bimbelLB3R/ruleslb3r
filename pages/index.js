5c270806a5f26c2246025fa788e95b0321f7382c
bimbelLB3R <bimbellb3r@gmail.com>
2025-02-17 13:34:54 +0800

    hide swal

--- pages/index.js
+++ pages/index.js
@@ -28,75 +28,63 @@ import VoiceControl from "../components/VoiceControl";
 const Home = ({ allPost, dataAlumni }) => {
   // console.log(allPost);
   const [currentIndex, setCurrentIndex] = useState(0);
+// batas swal info alumni lulus
+  // useEffect(() => {
+  //   // Fungsi pembersihan
+  //   let isMounted = true;
+  //   // Memunculkan nama user secara bergantian
+  //   const showUserName = (index) => {
+  //     if (!isMounted) return;
+  //     if (index >= dataAlumni.length) {
+  //       // Jika sudah mencapai akhir data, ulangi dari awal
+  //       setCurrentIndex(0);
+  //       return;
+  //     }
 
-  useEffect(() => {
-    // Fungsi pembersihan
-    let isMounted = true;
-    // Memunculkan nama user secara bergantian
-    const showUserName = (index) => {
-      if (!isMounted) return;
-      if (index >= dataAlumni.length) {
-        // Jika sudah mencapai akhir data, ulangi dari awal
-        setCurrentIndex(0);
-        return;
-      }
+  //     // Tampilkan alert SweetAlert dengan jeda waktu 1 detik
+  //     setTimeout(() => {
+  //       const Toast = Swal.mixin({
+  //         toast: true,
+  //         position: "bottom",
+  //         background: "#f5f3ff",
+  //         padding: "0.1rem",
+  //         showConfirmButton: false,
+  //         customClass: {
+  //           // Atur kelas CSS untuk mengatur ukuran teks (contoh: 'custom-swal-text')
+  //           content: "custom-swal-text",
+  //         },
+  //         timer: 3000,
+  //         timerProgressBar: true,
+  //         didOpen: (toast) => {
+  //           toast.addEventListener("mouseenter", Swal.stopTimer);
+  //           toast.addEventListener("mouseleave", Swal.resumeTimer);
+  //         },
+  //       });
 
-      // Tampilkan alert SweetAlert dengan jeda waktu 1 detik
-      setTimeout(() => {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "bottom",
-          background: "#f5f3ff",
-          padding: "0.1rem",
-          showConfirmButton: false,
-          customClass: {
-            // Atur kelas CSS untuk mengatur ukuran teks (contoh: 'custom-swal-text')
-            content: "custom-swal-text",
-          },
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
+  //       Toast.fire({
+  //         icon: "success",
+  //         title: "Lolos SNBT 2024",
+  //         text: dataAlumni[index].nama,
+  //       });
+        
 
-        Toast.fire({
-          icon: "success",
-          title: "Lolos SNBT 2024",
-          text: dataAlumni[index].nama,
-        });
-        // Swal.fire({
-        //   showConfirmButton: false,
-        //   timer: 1500,
-        //   position: "bottom",
-        //   showClass: {
-        //     popup: "animate__animated animate__fadeInUp",
-        //   },
-        //   hideClass: {
-        //     popup: "animate__animated animate__fadeOutDown",
-        //   },
-        //   backdrop: false,
-        //   padding: "0.1rem",
-        //   allowOutsideClick: false,
-        //   text: dataAlumni[index].nama,
-        // });
+  //       // Lanjutkan ke nama user berikutnya
+  //       if (isMounted) {
+  //         // Tambahkan ini
+  //         showUserName(index + 1);
+  //       }
+  //     }, 4000);
+  //   };
 
-        // Lanjutkan ke nama user berikutnya
-        if (isMounted) {
-          // Tambahkan ini
-          showUserName(index + 1);
-        }
-      }, 4000);
-    };
+  //   // Memulai rekursif untuk menampilkan nama user
+  //   showUserName(currentIndex);
+  //   // Fungsi pembersihan
+  //   return () => {
+  //     isMounted = false;
+  //   };
+  // }, [currentIndex]);
 
-    // Memulai rekursif untuk menampilkan nama user
-    showUserName(currentIndex);
-    // Fungsi pembersihan
-    return () => {
-      isMounted = false;
-    };
-  }, [currentIndex]);
+  // batas swal info alumni lulus
 
   // console.log(allPost);
   const [showNavbar, setShowNavbar] = useState(false);
