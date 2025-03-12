81d292f8ab285253e816881aed7bc66cdb09bb71
bimbelLB3R <bimbellb3r@gmail.com>
2025-03-12 18:08:19 +0800

    member premium

--- components/Loginmembersupa.js
+++ components/Loginmembersupa.js
@@ -183,7 +183,7 @@ const Loginmembersupa = () => {
               src="/image/logolb3r.png"
               alt="logo"
             />
-            SNBT Program
+            Premium Member
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -204,7 +204,7 @@ const Loginmembersupa = () => {
                 ""
               )}
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Simulasi UTBK SNBT
+                Premium Member
               </h1>
               <p>{erorKoneksi&&"Koneksi internet kamu kurang bagus..!!"}</p>
               <form
--- components/Newmembersup.js
+++ components/Newmembersup.js
@@ -195,7 +195,7 @@ export default function Newmembersup() {
               src="/image/logolb3r.png"
               alt="logo"
             />
-            SNBT Program
+            Member Premium
           </Link>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
