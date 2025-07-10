abeb76b17a1ed0c0deb4c56a31e6c37fe89caa27
bimbelLB3R <bimbellb3r@gmail.com>
2025-07-10 21:13:05 +0800

    ubah program

--- components/Welcome.js
+++ components/Welcome.js
@@ -14,7 +14,7 @@ const Welcome = () => {
             Bimbel LB3R sudah hadir di kabupaten Tabalong sejak tahun 2012 dan telah mendampingi ribuan siswa dari jenjang TK, SD, SMP, SMA hingga ALumni. LB3R selalu mengikuti perubahan kurikulum sehingga pembelajarannya selalu up date sesuai kebutuhan kamu. Di LB3R kamu bisa membuat kelas kamu sendiri dan mengatur jadwal secara fleksibel menyesuaikan waktu antara belajar di sekolah, berorganisasi, dan belajar di bimbel sehingga kamu bisa belajar dengan efektif dan efisien. Selain waktu belajar yang fleksibel, sistem pembayaran juga mudah dan terjangkau untuk semua kalangan. Orang tua juga akan mendapatkan laporan riwayat kehadiran dan pembayaran siswa serta bisa secara langsung berdiskusi tentang perkembangan anak.
           </p>
           <br/>
-          <p>Ada beberapa program di LB3R yang bertujuan untuk menambah ketrampilan anak seperti ketrampilan berbahasa asing dan koding. Selain itu, LB3R juga memberikan layanan asesmen bakat dan minat menggunakan talents mapping dan talents tracker untuk pemilihan jurusan serta pengembangan karir anak. Talents mapping dan talenta tracker juga bisa digunakan untuk mengatasi masalah kepribadian anak dan masalah-masalah lain terkait psikologis anak dalam hubungan keluarga dan sosial.</p>
+          <p>Ada beberapa program di LB3R yang bertujuan untuk menambah ketrampilan anak seperti ketrampilan berbahasa asing dan koding. Selain itu, untuk kelas 6,9 dan kelas 12 kami sediakan program TKA (Tes Kemampuan Akademik) pengganti Ujian Nasional. Bagi kelas 12 kami juga menyiapkan program persiapan UTBK SNBT dan Persiapan untuk sekolah kedinasan maupun ujian Mandiri.</p>
         </div>
       </div>
     </div>
